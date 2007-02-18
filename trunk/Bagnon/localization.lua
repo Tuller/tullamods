e4ee240d6cd2ed5c79595d690f47a52df0af7f9a
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-02-18 04:27:52 +0000

    Removed unneeded localization data

--- trunk/Bagnon/localization.lua
+++ trunk/Bagnon/localization.lua
@@ -97,20 +97,20 @@ BAGNON_SPOT_TOOLTIP = "<Double-Click> to search."
 --[[ Item types, the sixth return from GetItemInfo(id) ]]--
 
 BAGNON_TYPE = {}
-BAGNON_TYPE['Weapon'] = select(1, GetAuctionItemClasses())
-BAGNON_TYPE['Armor'] = select(2, GetAuctionItemClasses())
+--BAGNON_TYPE['Weapon'] = select(1, GetAuctionItemClasses())
+--BAGNON_TYPE['Armor'] = select(2, GetAuctionItemClasses())
 BAGNON_TYPE['Container'] = select(3, GetAuctionItemClasses())
-BAGNON_TYPE['Consumable'] = select(4, GetAuctionItemClasses())
-BAGNON_TYPE['Trade Goods'] = select(5, GetAuctionItemClasses())
-BAGNON_TYPE['Projectile'] = select(6, GetAuctionItemClasses())
+--BAGNON_TYPE['Consumable'] = select(4, GetAuctionItemClasses())
+--BAGNON_TYPE['Trade Goods'] = select(5, GetAuctionItemClasses())
+--BAGNON_TYPE['Projectile'] = select(6, GetAuctionItemClasses())
 BAGNON_TYPE['Quiver'] = select(7, GetAuctionItemClasses())
-BAGNON_TYPE['Recipe'] = select(8, GetAuctionItemClasses())
-BAGNON_TYPE['Reagent'] = select(9, GetAuctionItemClasses())
-BAGNON_TYPE['Miscellaneous'] = select(10, GetAuctionItemClasses())
+--BAGNON_TYPE['Recipe'] = select(8, GetAuctionItemClasses())
+--BAGNON_TYPE['Reagent'] = select(9, GetAuctionItemClasses())
+--BAGNON_TYPE['Miscellaneous'] = select(10, GetAuctionItemClasses())
 
 --types not listed in GetAuctionItemClasses
-BAGNON_TYPE['Quest'] = 'Quest'
-BAGNON_TYPE['Key'] = 'Key'
+--BAGNON_TYPE['Quest'] = 'Quest'
+--BAGNON_TYPE['Key'] = 'Key'
 
 
 --[[ Subtypes, the seventh return from GetItemInfo(id) ]]--
@@ -118,75 +118,75 @@ BAGNON_TYPE['Key'] = 'Key'
 BAGNON_SUBTYPE = {}
 
 --weapon
-BAGNON_SUBTYPE['One-Handed Axes'] = select(1, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Two-Handed Axes'] = select(2, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Bows'] = select(3, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Guns'] = select(4, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['One-Handed Maces'] = select(5, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Two-Handed Maces'] = select(6, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Polearms'] = select(7, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['One-Handed Swords'] = select(8, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Two-Handed Swords'] = select(9, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Staves'] = select(10, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Fist Weapons'] = select(11, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['One-Handed Axes'] = select(1, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Two-Handed Axes'] = select(2, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Bows'] = select(3, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Guns'] = select(4, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['One-Handed Maces'] = select(5, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Two-Handed Maces'] = select(6, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Polearms'] = select(7, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['One-Handed Swords'] = select(8, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Two-Handed Swords'] = select(9, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Staves'] = select(10, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Fist Weapons'] = select(11, GetAuctionItemSubClasses(1))
 --BAGNON_SUBTYPE['Miscellaneous'] = select(12, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Daggers'] = select(13, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Thrown'] = select(14, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Crossbows'] = select(15, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Wands'] = select(16, GetAuctionItemSubClasses(1))
-BAGNON_SUBTYPE['Fishing Pole'] = select(17, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Daggers'] = select(13, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Thrown'] = select(14, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Crossbows'] = select(15, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Wands'] = select(16, GetAuctionItemSubClasses(1))
+-- BAGNON_SUBTYPE['Fishing Pole'] = select(17, GetAuctionItemSubClasses(1))
 
 --armor
 --BAGNON_SUBTYPE['Miscellaneous'] = select(1, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Cloth'] = select(2, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Leather'] = select(3, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Mail'] = select(4, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Plate'] = select(5, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Shields'] = select(6, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Librams'] = select(7, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Idols'] = select(8, GetAuctionItemSubClasses(2))
-BAGNON_SUBTYPE['Totems'] = select(9, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Cloth'] = select(2, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Leather'] = select(3, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Mail'] = select(4, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Plate'] = select(5, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Shields'] = select(6, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Librams'] = select(7, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Idols'] = select(8, GetAuctionItemSubClasses(2))
+-- BAGNON_SUBTYPE['Totems'] = select(9, GetAuctionItemSubClasses(2))
 
 --bag
 BAGNON_SUBTYPE['Bag'] = select(1, GetAuctionItemSubClasses(3))
 BAGNON_SUBTYPE['Soul Bag'] = select(2, GetAuctionItemSubClasses(3))
-BAGNON_SUBTYPE['Herb Bag'] = select(3, GetAuctionItemSubClasses(3))
-BAGNON_SUBTYPE['Enchanting Bag'] = select(4, GetAuctionItemSubClasses(3))
-BAGNON_SUBTYPE['Engineering Bag'] = select(5, GetAuctionItemSubClasses(3))
-BAGNON_SUBTYPE['Gem Bag'] = select(6, GetAuctionItemSubClasses(3))
-BAGNON_SUBTYPE['Mining Bag'] = select(7, GetAuctionItemSubClasses(3))
+-- BAGNON_SUBTYPE['Herb Bag'] = select(3, GetAuctionItemSubClasses(3))
+-- BAGNON_SUBTYPE['Enchanting Bag'] = select(4, GetAuctionItemSubClasses(3))
+-- BAGNON_SUBTYPE['Engineering Bag'] = select(5, GetAuctionItemSubClasses(3))
+-- BAGNON_SUBTYPE['Gem Bag'] = select(6, GetAuctionItemSubClasses(3))
+-- BAGNON_SUBTYPE['Mining Bag'] = select(7, GetAuctionItemSubClasses(3))
 
 --projectile
-BAGNON_SUBTYPE['Arrow'] = select(1, GetAuctionItemSubClasses(6))
-BAGNON_SUBTYPE['Bullet'] = select(2, GetAuctionItemSubClasses(6))
+-- BAGNON_SUBTYPE['Arrow'] = select(1, GetAuctionItemSubClasses(6))
+-- BAGNON_SUBTYPE['Bullet'] = select(2, GetAuctionItemSubClasses(6))
 
 --quiver
-BAGNON_SUBTYPE['Quiver'] = select(1, GetAuctionItemSubClasses(7))
-BAGNON_SUBTYPE['Ammo Pouch'] = select(2, GetAuctionItemSubClasses(7))
+-- BAGNON_SUBTYPE['Quiver'] = select(1, GetAuctionItemSubClasses(7))
+-- BAGNON_SUBTYPE['Ammo Pouch'] = select(2, GetAuctionItemSubClasses(7))
 
 --recipe
-BAGNON_SUBTYPE['Book'] = select(1, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Leatherworking'] = select(2, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Tailoring'] = select(3, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Engineering'] = select(4, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Blacksmithing'] = select(5, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Cooking'] = select(6, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Alchemy'] = select(7, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['First Aid'] = select(8, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Enchanting'] = select(9, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Fishing'] = select(10, GetAuctionItemSubClasses(8))
-BAGNON_SUBTYPE['Jewelcrafting'] = select(11, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Book'] = select(1, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Leatherworking'] = select(2, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Tailoring'] = select(3, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Engineering'] = select(4, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Blacksmithing'] = select(5, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Cooking'] = select(6, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Alchemy'] = select(7, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['First Aid'] = select(8, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Enchanting'] = select(9, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Fishing'] = select(10, GetAuctionItemSubClasses(8))
+-- BAGNON_SUBTYPE['Jewelcrafting'] = select(11, GetAuctionItemSubClasses(8))
 
 --subtypes not listed in GetAuctionItemSubClasses
-BAGNON_SUBTYPE['Devices'] = 'Devices'
-BAGNON_SUBTYPE['Junk'] = 'Junk'
-BAGNON_SUBTYPE['Key'] = 'Key'
-BAGNON_SUBTYPE['Parts'] = 'Parts'
-BAGNON_SUBTYPE['Explosives'] = 'Explosives'
-BAGNON_SUBTYPE['Gems'] = 'Gems'
+-- BAGNON_SUBTYPE['Devices'] = 'Devices'
+-- BAGNON_SUBTYPE['Junk'] = 'Junk'
+-- BAGNON_SUBTYPE['Key'] = 'Key'
+-- BAGNON_SUBTYPE['Parts'] = 'Parts'
+-- BAGNON_SUBTYPE['Explosives'] = 'Explosives'
+-- BAGNON_SUBTYPE['Gems'] = 'Gems'
 
 
 --[[ Strings ]]--
 
-BAGNON_STRING_BANDAGE = 'bandage'
-BAGNON_STRING_POISON = 'poison'
+-- BAGNON_STRING_BANDAGE = 'bandage'
+-- BAGNON_STRING_POISON = 'poison'
