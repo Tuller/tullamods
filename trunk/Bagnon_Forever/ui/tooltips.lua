62b7f0542129bfdfa37e23c84d0d9ec1c2212459
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-03-13 20:27:00 +0000

    Added a fix to prevent adding information to tooltips we don't need to add information to.

--- trunk/Bagnon_Forever/ui/tooltips.lua
+++ trunk/Bagnon_Forever/ui/tooltips.lua
@@ -1,10 +1,10 @@
 --[[
-	Ludwig_SellValue -
-		Originally based on SellValueLite, this addon allows viewing of sellvalues
+	tooltips.lua
+		Adds ownership information to visible tooltips.
 --]]
 
 local function AddOwners(frame, id)
-	if not(frame and id and BagnonUtil:ShowingTooltips()) then return end
+	if not(id and BagnonUtil:ShowingTooltips()) then return end
 
 	for player in BagnonDB:GetPlayers() do
 		local invCount = BagnonDB:GetItemCount(id, -2, player)
@@ -46,34 +46,52 @@ end
 
 --[[  Function Hooks ]]--
 
+local function IsValidTooltip(frame)
+	return (frame == GameTooltip or frame == ItemRefTooltip) and frame:IsVisible()
+end
+
 GameTooltip.SetBagItem = pHook(GameTooltip.SetBagItem, function(self, bag, slot)
-	AddOwners(self, GetContainerItemLink(bag, slot))
+	if IsValidTooltip(self) then
+		AddOwners(self, GetContainerItemLink(bag, slot))
+	end
 end)
 
 GameTooltip.SetLootItem = pHook(GameTooltip.SetLootItem, function(self, slot)
-	AddOwners(self, GetLootSlotLink(slot))
+	if IsValidTooltip(self) then
+		AddOwners(self, GetLootSlotLink(slot))
+	end
 end)
 
 GameTooltip.SetHyperlink = pHook(GameTooltip.SetHyperlink, function(self, link)
-	AddOwners(self, link)
+	if IsValidTooltip(self) then
+		AddOwners(self, link)
+	end
 end)
 
 GameTooltip.SetLootRollItem = pHook(GameTooltip.SetLootRollItem, function(self, rollID)
-	AddOwners(self, GetLootRollItemLink(rollID))
+	if IsValidTooltip(self) then
+		AddOwners(self, GetLootRollItemLink(rollID))
+	end
 end)
 
 GameTooltip.SetAuctionItem = pHook(GameTooltip.SetAuctionItem, function(self, type, index)
-	AddOwners(self, GetAuctionItemLink(type, index))
+	if IsValidTooltip(self) then
+		AddOwners(self, GetAuctionItemLink(type, index))
+	end
 end)
 
 GameTooltip.SetQuestItem = pHook(GameTooltip.SetQuestItem, function(self, type, id)
-	AddOwners(self, GetQuestItemLink(type, id))
+	if IsValidTooltip(self) then
+		AddOwners(self, GetQuestItemLink(type, id))
+	end
 end)
 
 GameTooltip.SetTradeSkillItem = pHook(GameTooltip.SetTradeSkillItem, function(self, type, id)
-	if not id then
-		AddOwners(self, GetTradeSkillItemLink(type))
-	else
-		AddOwners(self, GetTradeSkillReagentItemLink(type, id))
+	if IsValidTooltip(self) then
+		if not id then
+			AddOwners(self, GetTradeSkillItemLink(type))
+		else
+			AddOwners(self, GetTradeSkillReagentItemLink(type, id))
+		end
 	end
 end)
