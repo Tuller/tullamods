5e7898f84e53e720a305ff217a1f5684b99a5e13
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-04-12 19:18:49 +0000

    Added Chinese translation (Diablohu)

--- trunk/Bagnon_Options/localization.cn.lua
+++ trunk/Bagnon_Options/localization.cn.lua
@@ -1,17 +1,31 @@
-﻿--[[ This File is Encoded in UTF-8 ]]--
-
---[[
+﻿--[[
 	Bagnon Main Options Localization file
 --]]
 
 --[[
-	Chinese    by   Diablohu
+	THIS FILE IS ENCODED IN UTF-8
+
+	Bagnon Localization file: Chinese 
+		Credit goes to Diablohu
+	
+	Last Update: 4.11.2007
 --]]
 
 if ( GetLocale() == "zhCN" ) then
 	local L = BAGNON_OPTIONS_LOCALS
+	
 	L.Title = "Bagnon 设置";
-	L.Show = "显示";
+
+	L.ShowOwners = "显示物品所有者"
+	L.ShowBorders = "显示物品质量框"
+
+	L.ReplaceBags = "替换默认背包界面"
+	L.ReplaceBank = "替换默认银行界面"
+	L.ReuseFrames = "重新使用默认界面"
+
+	L.Show = "显示"
+	L.Bags = "背包"
+	L.Bank = "银行"
 
 	L.AtBank = "在银行时";
 	L.AtVendor = "与商贩对话时";
@@ -19,5 +33,4 @@ if ( GetLocale() == "zhCN" ) then
 	L.AtMail = "打开邮箱时";
 	L.AtTrade = "交易时";
 	L.AtCraft = "制作物品时";
-	L.ShowBorders = "显示物品质量框";
 end
