3cc842cb0457cdfa83d8323652d438314549a601
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-05-26 17:20:55 +0000

    Modified /bagnon to show the main menu

--- trunk/Bagnon/main.lua
+++ trunk/Bagnon/main.lua
@@ -440,9 +440,10 @@ end
 
 function Bagnon:RegisterSlashCommands()
 	local slash = self:InitializeSlashCommand(L.Commands, "BAGNON", "bagnon", "bgn")
-	slash:RegisterSlashHandler(format("menu: %s", L.ShowMenuDesc), "menu", "ShowMenu")
-	slash:RegisterSlashHandler(format("bags: %s", L.ShowBagsDesc), "bags", "ToggleInventory")
-	slash:RegisterSlashHandler(format("bank: %s", L.ShowBankDesc), "bank", "ToggleBank")
+	slash:RegisterSlashHandler(format("/bagnon: %s", L.ShowMenuDesc), "", "ShowMenu")
+	slash:RegisterSlashHandler(format("menu: %s", L.ShowMenuDesc), "^menu$", "ShowMenu")
+	slash:RegisterSlashHandler(format("bags: %s", L.ShowBagsDesc), "^bags$", "ToggleInventory")
+	slash:RegisterSlashHandler(format("bank: %s", L.ShowBankDesc), "^bank$", "ToggleBank")
 	self.slash = slash
 end
 
