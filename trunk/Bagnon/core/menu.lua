d254d1d58e2704d205bee12e97939f299fcff5f8
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-05-26 17:15:13 +0000

    Fixed an error when setting the background color

--- trunk/Bagnon/core/menu.lua
+++ trunk/Bagnon/core/menu.lua
@@ -3,8 +3,8 @@
 		Functions for the Bagnon right click options menu
 --]]
 
-local STRATAS = {'LOW', 'MEDIUM', 'HIGH'}
-local MENU_NAME = 'BagnonMenu'
+local STRATAS = {"LOW", "MEDIUM", "HIGH"}
+local MENU_NAME = "BagnonMenu"
 local L = BAGNON_LOCALS
 
 local function ToIndex(value, items)
@@ -37,16 +37,16 @@ local function Slider_OnValueChanged(self, value)
 	end
 	
 	if values then
-		getglobal(self:GetName() .. 'ValText'):SetText(values[value])
+		getglobal(self:GetName() .. "ValText"):SetText(values[value])
 	else
-		getglobal(self:GetName() .. 'ValText'):SetText(value)
+		getglobal(self:GetName() .. "ValText"):SetText(value)
 	end
 end
 
 local function Slider_Create(parent, suffix, minVal, maxVal, step, title, OnChange, isPercent, values)
 	local name = parent:GetName() .. suffix
 
-	local slider = CreateFrame('Slider', name, parent, 'GooeySlider')
+	local slider = CreateFrame("Slider", name, parent, "GooeySlider")
 	slider:SetWidth(216); slider:SetHeight(18)
 	slider.OnChange = OnChange
 	slider.isPercent = isPercent
@@ -54,15 +54,15 @@ local function Slider_Create(parent, suffix, minVal, maxVal, step, title, OnChan
 	
 	slider:SetValueStep(step)
 	slider:SetMinMaxValues(minVal, maxVal)
-	slider:SetScript('OnValueChanged', Slider_OnValueChanged)
+	slider:SetScript("OnValueChanged", Slider_OnValueChanged)
 	
-	getglobal(name .. 'Text'):SetText(title)
+	getglobal(name .. "Text"):SetText(title)
 	if values then
-		getglobal(name .. 'Low'):SetText(values[minVal])
-		getglobal(name .. 'High'):SetText(values[maxVal])
+		getglobal(name .. "Low"):SetText(values[minVal])
+		getglobal(name .. "High"):SetText(values[maxVal])
 	else
-		getglobal(name .. 'Low'):SetText(minVal)
-		getglobal(name .. 'High'):SetText(maxVal)
+		getglobal(name .. "Low"):SetText(minVal)
+		getglobal(name .. "High"):SetText(maxVal)
 	end
 
 	return slider
@@ -83,7 +83,7 @@ local function BGColor_OnColorChange()
 	
 	frame:SetBackdropColor(r, g, b, a)
 	frame:SetBackdropBorderColor(1, 1, 1, a)
-	getglobal(MENU_NAME .. 'BGColorNormalTexture'):SetVertexColor(r, g, b, a)
+	getglobal(MENU_NAME .. "BGColorNormalTexture"):SetVertexColor(r, g, b, a)
 end
 
 local function BGColor_OnAlphaChange()
@@ -95,7 +95,7 @@ local function BGColor_OnAlphaChange()
 
 	frame:SetBackdropColor(bgSets.r, bgSets.g, bgSets.b, alpha)
 	frame:SetBackdropBorderColor(1, 1, 1, alpha)
-	getglobal(MENU_NAME .. 'BGColorNormalTexture'):SetVertexColor(bgSets.r, bgSets.g, bgSets.b, alpha)
+	getglobal(MENU_NAME .. "BGColorNormalTexture"):SetVertexColor(bgSets.r, bgSets.g, bgSets.b, alpha)
 end
 
 local function BGColor_CancelChanges() 
@@ -110,7 +110,7 @@ local function BGColor_CancelChanges()
 	
 	frame:SetBackdropColor(prevValues.r, prevValues.g, prevValues.b, prevValues.opacity)
 	frame:SetBackdropBorderColor(1, 1, 1, prevValues.opacity)
-	getglobal(MENU_NAME .. 'BGColorNormalTexture'):SetVertexColor(prevValues.r, prevValues.g, prevValues.b, prevValues.opacity)
+	getglobal(MENU_NAME .. "BGColorNormalTexture"):SetVertexColor(prevValues.r, prevValues.g, prevValues.b, prevValues.opacity)
 end
 
 local function BGColor_OnClick(frame)
@@ -130,7 +130,7 @@ local function BGColor_OnClick(frame)
 		
 		ColorPickerFrame:SetColorRGB(bgSets.r, bgSets.g, bgSets.b)
 		
-		getglobal(MENU_NAME .. 'BGColorNormalTexture'):SetVertexColor(bgSets.r, bgSets.g, bgSets.b, bgSets.a)
+		getglobal(MENU_NAME .. "BGColorNormalTexture"):SetVertexColor(bgSets.r, bgSets.g, bgSets.b, bgSets.a)
 		
 		ShowUIPanel(ColorPickerFrame)
 	end
@@ -168,7 +168,7 @@ local function SetFrameScale(frame, scale)
 
 	frame:SetScale(scale)
 	frame:ClearAllPoints()
-	frame:SetPoint('TOPLEFT', UIParent, 'BOTTOMLEFT', x, y)
+	frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", x, y)
 	frame:SavePosition()
 end
 
@@ -199,56 +199,56 @@ end
 --[[ Menu Constructor ]]--
 
 local function CreateMenu(name)
-	local menu = CreateFrame('Button', name, UIParent, 'GooeyPopup')
+	local menu = CreateFrame("Button", name, UIParent, "GooeyPopup")
 	menu:SetWidth(230); menu:SetHeight(378)
-	menu:SetScript("OnHide", function() this.anchor = nil; end)
-	menu:SetScript("OnClick", function() this:Hide() end)
+	menu:SetScript("OnHide", function(self) self.anchor = nil; end)
+	menu:SetScript("OnClick", function(self) self:Hide() end)
 	menu:RegisterForClicks("anyUp")
 	
-	local title = menu:CreateFontString(name .. 'Title', 'ARTWORK')
-	title:SetFontObject('GameFontHighlightLarge')
-	title:SetPoint('TOP', menu, 'TOP', 0, -8)
+	local title = menu:CreateFontString(name .. "Title", "ARTWORK")
+	title:SetFontObject("GameFontHighlightLarge")
+	title:SetPoint("TOP", menu, "TOP", 0, -8)
 	title:SetText(L.FrameSettings)
 	
-	local close = CreateFrame('Button', name .. 'Close', menu, 'UIPanelCloseButton')
-	close:SetPoint('TOPRIGHT', menu, 'TOPRIGHT', -1, -1)
+	local close = CreateFrame("Button", name .. "Close", menu, "UIPanelCloseButton")
+	close:SetPoint("TOPRIGHT", menu, "TOPRIGHT", -1, -1)
 	
 	--checkbuttons
-	local lock = CreateFrame('CheckButton', name .. 'Lock', menu, 'GooeyCheckButton')
-	lock:SetPoint('TOPLEFT', menu, 'TOPLEFT', 6, -30)
-	lock:SetScript('OnClick', function(self) menu.anchor:Lock(self:GetChecked()) end)
+	local lock = CreateFrame("CheckButton", name .. "Lock", menu, "GooeyCheckButton")
+	lock:SetPoint("TOPLEFT", menu, "TOPLEFT", 6, -30)
+	lock:SetScript("OnClick", function(self) menu.anchor:Lock(self:GetChecked()) end)
 	lock:SetText(L.Lock)
 	
-	local reverse = CreateFrame('CheckButton', name .. 'Reverse', menu, 'GooeyCheckButton')
-	reverse:SetPoint('TOPLEFT', lock, 'BOTTOMLEFT')
-	reverse:SetScript('OnClick', function(self) SetReverseSort(menu.anchor, self:GetChecked()) end)
+	local reverse = CreateFrame("CheckButton", name .. "Reverse", menu, "GooeyCheckButton")
+	reverse:SetPoint("TOPLEFT", lock, "BOTTOMLEFT")
+	reverse:SetScript("OnClick", function(self) SetReverseSort(menu.anchor, self:GetChecked()) end)
 	reverse:SetText(L.ReverseSort)
 	
-	local topLevel = CreateFrame('CheckButton', name .. 'TopLevel', menu, 'GooeyCheckButton')
-	topLevel:SetPoint('TOPLEFT', reverse, 'BOTTOMLEFT')
-	topLevel:SetScript('OnClick', function(self) SetToplevel(menu.anchor, self:GetChecked()) end)
+	local topLevel = CreateFrame("CheckButton", name .. "TopLevel", menu, "GooeyCheckButton")
+	topLevel:SetPoint("TOPLEFT", reverse, "BOTTOMLEFT")
+	topLevel:SetScript("OnClick", function(self) SetToplevel(menu.anchor, self:GetChecked()) end)
 	topLevel:SetText(L.Toplevel)
 	
-	local colorPicker = CreateFrame('Button', name .. 'BGColor', menu, 'GooeyColorPicker')
-	colorPicker:SetPoint('TOPLEFT', topLevel, 'BOTTOMLEFT', 4, 2)
-	colorPicker:SetScript('OnClick', function(self) BGColor_OnClick(self.anchor) end)
+	local colorPicker = CreateFrame("Button", name .. "BGColor", menu, "GooeyColorPicker")
+	colorPicker:SetPoint("TOPLEFT", topLevel, "BOTTOMLEFT", 4, 2)
+	colorPicker:SetScript("OnClick", function() BGColor_OnClick(menu.anchor) end)
 	colorPicker:SetText(L.BackgroundColor)
 
 	--sliders
-	local strata = Slider_Create(menu, 'Level', 1, 3, 1, L.FrameLevel, SetFrameStrata, nil, STRATAS)
-	strata:SetPoint('BOTTOM', menu, 'BOTTOM', 0, 20)
+	local strata = Slider_Create(menu, "Level", 1, 3, 1, L.FrameLevel, SetFrameStrata, nil, STRATAS)
+	strata:SetPoint("BOTTOM", menu, "BOTTOM", 0, 20)
 	
-	local alpha = Slider_Create(menu, 'Opacity', 0, 100, 1, L.Opacity, SetFrameAlpha, true)
-	alpha:SetPoint('BOTTOM', strata, 'BOTTOM', 0, 40)
+	local alpha = Slider_Create(menu, "Opacity", 0, 100, 1, L.Opacity, SetFrameAlpha, true)
+	alpha:SetPoint("BOTTOM", strata, "BOTTOM", 0, 40)
 	
-	local scale = Slider_Create(menu, 'Scale', 50, 150, 1, L.Scale, SetFrameScale, true)
-	scale:SetPoint('BOTTOM', alpha, 'BOTTOM', 0, 40)
+	local scale = Slider_Create(menu, "Scale", 50, 150, 1, L.Scale, SetFrameScale, true)
+	scale:SetPoint("BOTTOM", alpha, "BOTTOM", 0, 40)
 	
-	local space = Slider_Create(menu, 'Spacing', 0, 32, 1, L.Spacing, SetFrameSpacing)
-	space:SetPoint('BOTTOM', scale, 'BOTTOM', 0, 40)
+	local space = Slider_Create(menu, "Spacing", 0, 32, 1, L.Spacing, SetFrameSpacing)
+	space:SetPoint("BOTTOM", scale, "BOTTOM", 0, 40)
 	
-	local cols = Slider_Create(menu, 'Cols', 4, 40, 1, L.Cols, SetFrameColumns)
-	cols:SetPoint('BOTTOM', space, 'BOTTOM', 0, 40)
+	local cols = Slider_Create(menu, "Cols", 4, 40, 1, L.Cols, SetFrameColumns)
+	cols:SetPoint("BOTTOM", space, "BOTTOM", 0, 40)
 	
 	return menu
 end
@@ -273,21 +273,21 @@ function BagnonMenu:Show(frame)
 	local sets = frame.sets
 
 	--Set values
-	getglobal(MENU_NAME .. 'Lock'):SetChecked(frame:IsLocked())
-	getglobal(MENU_NAME .. 'TopLevel'):SetChecked(sets.topLevel)
-	getglobal(MENU_NAME .. 'Reverse'):SetChecked(sets.reverseSort)
+	getglobal(MENU_NAME .. "Lock"):SetChecked(frame:IsLocked())
+	getglobal(MENU_NAME .. "TopLevel"):SetChecked(sets.topLevel)
+	getglobal(MENU_NAME .. "Reverse"):SetChecked(sets.reverseSort)
 
 	local r, g, b, a = frame:GetBackgroundColor()
-	getglobal(MENU_NAME .. 'BGColorNormalTexture'):SetVertexColor(r, g, b, a)
+	getglobal(MENU_NAME .. "BGColorNormalTexture"):SetVertexColor(r, g, b, a)
 
 	local cols, spacing = frame:GetLayout()
-	getglobal(MENU_NAME .. 'Cols'):SetValue(cols)
-	getglobal(MENU_NAME .. 'Spacing'):SetValue(spacing)
-	getglobal(MENU_NAME .. 'Scale'):SetValue(frame:GetScale() * 100)
-	getglobal(MENU_NAME .. 'Opacity'):SetValue(frame:GetAlpha() * 100)
-	getglobal(MENU_NAME .. 'Level'):SetValue(ToIndex(frame:GetFrameStrata(), STRATAS))
+	getglobal(MENU_NAME .. "Cols"):SetValue(cols)
+	getglobal(MENU_NAME .. "Spacing"):SetValue(spacing)
+	getglobal(MENU_NAME .. "Scale"):SetValue(frame:GetScale() * 100)
+	getglobal(MENU_NAME .. "Opacity"):SetValue(frame:GetAlpha() * 100)
+	getglobal(MENU_NAME .. "Level"):SetValue(ToIndex(frame:GetFrameStrata(), STRATAS))
 
-	--place the frame at the player's cursor
+	--place the frame at the player"s cursor
 	local x, y = GetCursorPosition()
 	x = x / UIParent:GetScale()
 	y = y / UIParent:GetScale()
