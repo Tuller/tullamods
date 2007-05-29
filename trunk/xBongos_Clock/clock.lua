dc08f97a54186884b5f3f4dbfb97e437986562f5
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-05-29 21:22:45 +0000

    removed background

--- trunk/xBongos_Clock/clock.lua
+++ trunk/xBongos_Clock/clock.lua
@@ -3,57 +3,32 @@
 		A digital clock for bongos, loosely based on myClock
 --]]
 
+BongosClock = Bongos:NewModule("Bongos-Clock")
+
 local offset = 0
-local is24Hours
 local UPDATE_DELAY = 30
 
-local function UpdateTime(text, hours, minutes)
-	hours = mod(hours + offset, 24)
+local function Clock_UpdateTime(text, hours, minutes)
+	local hours = mod(hours + offset, 24)
+	local pm = hours >= 12
+	hours = mod(hours-1, 12) + 1
 
-	if is24Hours then
-		text:SetText(format(TEXT(TIME_TWENTYFOURHOURS), hours, minutes));
+	if pm then
+		text:SetText(format(TEXT(TIME_TWELVEHOURPM), hours, minutes))
 	else
-		local pm = hours >= 12
-		hours = math.fmod(hours, 12)
-		if hours == 0 then
-			hours = 12
-		end
-
-		if pm then
-			text:SetText(format(TEXT(TIME_TWELVEHOURPM), hours, minutes))
-		else
-			text:SetText(format(TEXT(TIME_TWELVEHOURAM), hours, minutes))
-		end
+		text:SetText(format(TEXT(TIME_TWELVEHOURAM), hours, minutes))
 	end
 end
 
-local function OnUpdate(self, elapsed)
-	if  self.elapsed <= 0 then
+local function Clock_OnUpdate(self, elapsed)
+	if self.elapsed <= 0 then
 		self.elapsed = UPDATE_DELAY
 
 		local hours, minutes = GetGameTime()
-		UpdateTime(self.text, hours, minutes)
+		Clock_UpdateTime(self.text, hours, minutes)
 	else
 		self.elapsed = self.elapsed - elapsed
 	end
-end
-
-local function Clock_Create(parent)
-	local clock = CreateFrame("Frame", "BongosClock", parent)
-	clock:SetAllPoints(parent)
-
-	clock.text = clock:CreateFontString(nil, "OVERLAY")
-	text:SetFontObject("GameFontNormalSmall")
-	text:SetAllPoints(clock)
-	
-	clock:SetBackdrop({
-	  bgFile = "Interface\\TutorialFrame\\TutorialFrameBackground", 
-	  edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", tile = true, tileSize = 16, edgeSize = 16, 
-	  insets = {left = 4, right = 4, top = 4, bottom = 4},
-	})
-	
-	clock.elapsed = 0
-	clock:SetScript("OnUpdate", OnUpdate)
 end
 
 
@@ -61,14 +36,25 @@ end
 
 local function Bar_OnCreate(self)
 	self:SetSize(70, 24)
-	Clock_Create(self)
+
+	local clock = CreateFrame("Frame", nil, self)
+	clock:SetAllPoints(self)
+
+	local text = clock:CreateFontString(nil, "OVERLAY")
+	text:SetFontObject("GameFontNormalSmall")
+	text:SetAllPoints(clock)
+	clock.text = text
+
+	clock.elapsed = 0
+	clock:SetScript("OnUpdate", Clock_OnUpdate)
 end
 
 function BongosClock:Load()
-	self.bar = BBar:Create("clock", Bar_OnCreate) 
-	if not self.bar:IsUserPlaced() then
-		self.bar:SetPoint("TOPRIGHT", UIParent)
+	local bar = BBar:Create("clock", Bar_OnCreate)
+	if not bar:IsUserPlaced() then
+		bar:SetPoint("TOPRIGHT", UIParent)
 	end
+	self.bar = bar
 end
 
 function BongosClock:Unload()
