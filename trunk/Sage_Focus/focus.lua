5a8a6814fdd76a0ca078a3151c5fb7ef8e814706
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-06-10 21:31:43 +0000

    Updated for 2.1 Sage

--- trunk/Sage_Focus/focus.lua
+++ trunk/Sage_Focus/focus.lua
@@ -1,19 +1,9 @@
 --[[
 	Sage Target
 		A Target Frame based on Sage
-
-	Unit Reaction Colors (r, g, b):
-		1 = {1, 0, 0}
-		2 = {1, 0, 0}
-		3 = {1, 0.5, 0}
-		4 = {1, 1, 0}
-		5 = {0, 1, 0}
-		6 = {0, 1, 0}
-		7 = {0, 1, 0}
-
-	TODO
-		Streamline the buff and debuff layout code
 --]]
+
+SageFocus = Sage:NewModule("Sage-Focus")
 
 local BUFF_SIZE_LARGE = 30
 local BUFF_SIZE_SMALL = 16
@@ -21,51 +11,24 @@ local BUFF_SIZE_SMALL = 16
 
 --[[ UI Templates ]]--
 
---npc info frame
---update the unit's level/name/class/type/faction
-local function NPCInfo_Update(self, unit)
-	self.class:SetText(SageInfo.GetClass(unit))
-	self.type:SetText(SageInfo.GetClassification(unit))
-end
-
-local function NPCInfo_Create(parent)
-	local npc = CreateFrame('Frame', nil, parent)
-	npc:SetAlpha(parent:GetAlpha())
-
-	npc.class = npc:CreateFontString(nil, 'OVERLAY')
-	npc.class:SetFontObject(SageFontSmall)
-	npc.class:SetJustifyH('LEFT')
-	npc.class:SetPoint('LEFT', npc)
-	
-	npc.type = npc:CreateFontString(nil, 'OVERLAY')
-	npc.type:SetFontObject(SageFontSmall)
-	npc.type:SetJustifyH('RIGHT')
-	npc.type:SetPoint('RIGHT', npc)	
-
-	npc.Update = NPCInfo_Update
-
-	return npc
-end
-
-
---[[ Update Functions ]]--
-
+--buff frame
 local function BuffFrame_PlaceLeft(self)
 	local left = self:GetParent().leftContainer
-	self:SetPoint('TOPLEFT', left)
+	self:SetPoint("TOPLEFT", left)
 	self:SetWidth(left:GetWidth())
-	self:SetHeight(left:GetHeight())	
+	self:SetHeight(left:GetHeight())
 end
 
 local function BuffFrame_PlaceBottom(self)
 	local bottom = self:GetParent().bottomContainer
-	self:SetPoint('TOPLEFT', bottom)
+	self:SetPoint("TOPLEFT", bottom)
 	self:SetWidth(bottom:GetWidth())
-	self:SetHeight(bottom:GetHeight())	
+	self:SetHeight(bottom:GetHeight())
 end
 
-local function LayoutBuffs(self, count)
-	if UnitIsFriend('player', self.id) then
+--main frame
+local function Frame_LayoutBuffs(self, count)
+	if UnitIsFriend("player", self.id) then
 		BuffFrame_PlaceBottom(self)
 	else
 		BuffFrame_PlaceLeft(self)
@@ -73,8 +36,8 @@ local function LayoutBuffs(self, count)
 	self:LayoutIcons(count)
 end
 
-local function LayoutDebuffs(self, count)
-	if UnitIsFriend('player', self.id) then
+local function Frame_LayoutDebuffs(self, count)
+	if UnitIsFriend("player", self.id) then
 		BuffFrame_PlaceLeft(self)
 	else
 		BuffFrame_PlaceBottom(self)
@@ -82,8 +45,8 @@ local function LayoutDebuffs(self, count)
 	self:LayoutIcons(count)
 end
 
---hides the manabar if its not mana, and an NPC, because NPC's only use mana
-local function UpdateManaBar(self)
+--hides the manabar if its not mana, and an NPC, because NPC"s only use mana
+local function Frame_UpdateManaBar(self)
 	if not UnitIsPlayer(self.id) and UnitPowerType(self.id) ~= 0 then
 		self.mana:Hide()
 		self.npc:SetPoint("TOPLEFT",  self.health, "BOTTOMLEFT")
@@ -96,116 +59,123 @@ local function UpdateManaBar(self)
 end
 
 --update everything about the target
-local function UpdateAll(self)
-	local unit = self.id	
+local function Frame_Update(self)
+	local unit = self.id
 
-	UpdateManaBar(self)
-	self.npc:Update(unit)
-
+	self:UpdateManaBar()
+	self.npc:Update()
 	self.info:UpdateAll()
 	self.info:UpdateNameColor()
 	self.health:UpdateAll()
 	self.mana:Update()
 	self.buff:Update()
 	self.debuff:Update()
+	self.cast:Update()
 end
 
 
---[[ Event Hander ]]--
-
-local function OnEvent()
-	if event == "PLAYER_FOCUS_CHANGED" then
-		UpdateAll(this)
-		CloseDropDownMenus()
-	elseif this:IsShown() then
-		if arg1 == this.id then
-			if event == "UNIT_CLASSIFICATION_CHANGED" then
-				this.npc:Update(arg1)
-			elseif event == "UNIT_FACTION" then
-				this.info:UpdateNameColor()
-			end
-		end
-	end
-end
-
-
---[[ Startup ]]--
-
-local function AddEvents(self)
-	self:SetScript("OnEvent", OnEvent)
-	self:RegisterEvent("PLAYER_FOCUS_CHANGED")
-	self:RegisterEvent("UNIT_FACTION")
-	self:RegisterEvent("UNIT_CLASSIFICATION_CHANGED")
-end
+--[[ Startup Functions ]]--
 
 --adds all the stuff to the target frame, this function should only be done once
-local function AddBars(self)
+local function Frame_AddBars(self)
+	self.UpdateManaBar = Frame_UpdateManaBar
+	self.Update = Frame_Update
+
 	--anchor point for placing buff/debuff frames
-	self.leftContainer = CreateFrame('FRAME', nil, self)
-	self.leftContainer:SetPoint('TOPRIGHT', self, 'TOPRIGHT', 0, -20)
+	self.leftContainer = CreateFrame("Frame", nil, self)
+	self.leftContainer:SetPoint("TOPRIGHT", self, "TOPRIGHT", 0, -20)
 	self.leftContainer:SetWidth(BUFF_SIZE_LARGE*2)
 	self.leftContainer:SetHeight(BUFF_SIZE_LARGE)
-
 	self.extraWidth = (self.extraWidth or 0) + self.leftContainer:GetWidth()
 
 	--unit name, health percentage, and level
-	self.info = SageInfo.Create(self)
-	self.info:SetPoint('TOPLEFT', self)
-	self.info:SetPoint('BOTTOMRIGHT', self.leftContainer, 'TOPLEFT')
+	self.info = SageInfo:Create(self, nil, true)
+	self.info:SetPoint("TOPLEFT", self)
+	self.info:SetPoint("BOTTOMRIGHT", self.leftContainer, "TOPLEFT")
 
 	--unit health
-	self.health = SageHealth.Create(self)
-	self.health:SetPoint('TOPLEFT', self.info, 'BOTTOMLEFT')
-	self.health:SetPoint('TOPRIGHT', self.info, 'BOTTOMRIGHT')
+	self.health = SageHealth:Create(self)
+	self.health:SetPoint("TOPLEFT", self.info, "BOTTOMLEFT")
+	self.health:SetPoint("TOPRIGHT", self.info, "BOTTOMRIGHT")
 	self.health:SetHeight(20)
 
-	local health_OnValueChanged = self.health:GetScript('OnValueChanged')
-	self.health:SetScript('OnValueChanged', function() health_OnValueChanged() self.info:UpdateNameColor() end)
+	local OnValueChanged = self.health:GetScript("OnValueChanged")
+	self.health:SetScript("OnValueChanged", function(self, value)
+		OnValueChanged(self, value)
+		self:GetParent().info:UpdateNameColor()
+	end)
 
 	--unit mana
-	self.mana = SageMana.Create(self)
-	self.mana:SetPoint('TOPLEFT', self.health, 'BOTTOMLEFT')
-	self.mana:SetPoint('TOPRIGHT', self.health, 'BOTTOMRIGHT')
+	self.mana = SageMana:Create(self)
+	self.mana:SetPoint("TOPLEFT", self.health, "BOTTOMLEFT")
+	self.mana:SetPoint("TOPRIGHT", self.health, "BOTTOMRIGHT")
 	self.mana:SetHeight(12)
 
 	--unit class, classification, and elite status
-	self.npc = NPCInfo_Create(self)
-	self.npc:SetPoint('TOPLEFT', self.mana, 'BOTTOMLEFT', 0, -1)
-	self.npc:SetPoint('TOPRIGHT', self.mana, 'BOTTOMRIGHT', 0, -1)
+	self.npc = SageNPC:Create(self)
+	self.npc:SetPoint("TOPLEFT", self.mana, "BOTTOMLEFT")
+	self.npc:SetPoint("TOPRIGHT", self.mana, "BOTTOMRIGHT")
 	self.npc:SetHeight(12)
 
 	--anchor point for placing buff/debuff frames
-	self.bottomContainer = CreateFrame('Frame', nil, self)
-	self.bottomContainer:SetPoint('TOPLEFT', self.npc, 'BOTTOMLEFT')
+	self.bottomContainer = CreateFrame("Frame", nil, self)
+	self.bottomContainer:SetPoint("TOPLEFT", self.npc, "BOTTOMLEFT")
 	self.bottomContainer:SetWidth(BUFF_SIZE_SMALL * 8)
 	self.bottomContainer:SetHeight(BUFF_SIZE_SMALL)
 
-	self.buff   = SageBuff.Create(self, LayoutBuffs)
-	self.debuff = SageDebuff.Create(self, LayoutDebuffs)
+	self.buff   = SageBuff:Create(self, nil, Frame_LayoutBuffs)
+	self.debuff = SageBuff:Create(self, nil, Frame_LayoutDebuffs, true)
 
-	if self.sets.showCombatText then 
-		SageCombatText.Register(self)
+	if self.sets.showCombatText then
+		SageCombat:Register(self)
 	end
-	
-	self.click:SetPoint('TOPLEFT', self.info)
-	self.click:SetPoint('BOTTOMRIGHT', self.npc)
+
+	self.click:SetPoint("TOPLEFT", self.info)
+	self.click:SetPoint("BOTTOMRIGHT", self.npc)
+
+	self.cast = SageCast:Create(self)
+	self.cast:SetAllPoints(self.npc)
 
 	self:SetHeight(BUFF_SIZE_SMALL + 13 + 12 + 20 + 20)
+	self:SetScript("OnReceiveDrag", TargetFrame_OnReceiveDrag)
 end
 
--- Called when the frame is actually created, this is the time to add any bars and other stuff to it
-local function OnCreate(self)
-	AddBars(self)
+function SageFocus:Load()
+	local defaults = {
+		combatTextSize = 24,
+		showCombatText = 1,
+		minWidth = 100,
+		x = 159.434,
+		y = 1097.579
+	}
+
+	self.frame = SageFrame:Create("focus", Frame_AddBars, defaults)
+	self.frame.info:UpdateWidth()
+
+	self:RegisterEvent("PLAYER_FOCUS_CHANGED")
+	-- self:RegisterEvent("UNIT_FACTION")
+	-- self:RegisterEvent("UNIT_CLASSIFICATION_CHANGED")
 end
 
--- This function's called when either a profile is loaded, or when Sage first starts up
-Sage.AddStartup(function()
-	if not Sage.GetFrameSets('focus') then
-		Sage.SetFrameSets('focus', {combatTextSize = 24, showCombatText = 1, minWidth = 100, x = 159.434, y = 1097.579})
-	end
+function SageFocus:Unload()
+	self:UnregisterAllEvents()
+	self.frame:Destroy()
+end
 
-	local frame = SageFrame.Create('focus', OnCreate)
-	frame.info:UpdateWidth()
-	AddEvents(frame)
-	UpdateAll(frame)
-end)
+function SageFocus:PLAYER_FOCUS_CHANGED()
+	self.frame:Update()
+end
+
+-- function SageFocus:UNIT_CLASSIFICATION_CHANGED(unit)
+	-- local frame = self.frame
+	-- if(unit == frame.id and frame:IsShown()) then
+		-- frame.npc:Update(unit)
+	-- end
+-- end
+
+-- function SageFocus:UNIT_FACTION(unit)
+	-- local frame = self.frame
+	-- if(unit == frame.id and frame:IsShown()) then
+		-- frame.npc:UpdateNameColor()
+	-- end
+-- end
