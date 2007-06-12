fbfcac5e9b84dc64ce9f22e368a9aebee1b56058
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-06-12 21:17:52 +0000

    basic menu written for 2.1 Sage

--- trunk/Sage_Options/menu.lua
+++ trunk/Sage_Options/menu.lua
@@ -1,62 +1,221 @@
 --[[
-		menu.lua
-			A GUI options menu for Sage
+	SageOptions.lua
+		GUI config functions for Bongos
 --]]
 
-local currentFrameName = SAGE_PANEL_GENERAL
+SageOptions = CreateFrame("Frame", "SageOptions", UIParent)
+local L = SAGE_LOCALS
+L.General = "General"
+L.Profiles = "Profiles"
+L.SetLock = "Lock Frame Positions"
+L.SetSticky = "Enable Sticky Frames"
+L.EnterName = "Enter Profile Name"
+L.OutlineBarText = "Outline Statusbar Text"
+L.OutlineOutside = "Outline Outside Text"
+L.HealthBarDebuffColoring = "Color Health When Debuffed"
+L.ShowHealthPercents = "Show Health Percents"
+L.ShowStatusBarText = "Show Statusbar Text"
+L.ShowCastBars = "Show Casting Bars"
 
-local function UpdateTabs()
-	local prevShown
-	local numShown = 0
+function SageOptions:Load()
+	--mother frame, used to hide and show the entire window
+	self:SetMovable(true)
+	self:SetClampedToScreen(true)
+	self:SetFrameStrata("DIALOG")
+	self:SetPoint("LEFT", UIParent)
 
-	for i = 1, select('#', SageOptionsMenu:GetChildren()) do
-		local frame = select(i, SageOptionsMenu:GetChildren())
-		if frame:IsShown() then
-			numShown = numShown + 1
-			frame:ClearAllPoints()
-			if prevShown then
-				frame:SetPoint("TOP", prevShown, "BOTTOM")
-			else
-				frame:SetPoint("TOPRIGHT", SageOptionsMenu, "TOPRIGHT", -8, -4)
-			end
-			prevShown = frame
-		end
-	end
-	SageOptionsMenu:SetHeight(12 + 26 * numShown)
+	local menu = self:AddMenu()
+	menu:SetPoint("TOPLEFT", self, "TOPLEFT", 0, -12)
+
+	local content = self:AddContentPane()
+	content:SetPoint("TOPLEFT", menu, "TOPRIGHT", -4, 12)
+	content:SetPoint("BOTTOMRIGHT", self)
+
+	self:AddGeneralPanel()
 end
 
-function SOptions_OnShow()
-	UpdateTabs()
-
-	currentFrame = getglobal("SageOptionsPanel" .. currentFrameName)
-	if not currentFrame:IsShown() then
-		currentFrame:Show()
-		getglobal("SageOptionsMenu" .. currentFrameName):LockHighlight()
-	end
-end
-
-function SOptions_SwitchTab(newFrameName)
-	if newFrameName ~= currentFrameName then
-		this:LockHighlight()
-		getglobal("SageOptionsMenu" .. currentFrameName):UnlockHighlight()
-		getglobal("SageOptionsPanel" .. currentFrameName):Hide()
-		getglobal("SageOptionsPanel" .. newFrameName):Show()
-		currentFrameName = newFrameName
+function SageOptions:Toggle()
+	if(self:IsShown()) then
+		self:Hide()
+	else
+		self:Show()
 	end
 end
 
-function SOptions_CreatePanel(name, title, OnShow)
-	assert(not getglobal("SageOptionsPanel" .. name), "SageOptionsPanel" .. name .. " already exists")
+--background frame for all the panels
+function SageOptions:AddContentPane()
+	--content pane, where all the panels are anchored to
+	local content = CreateFrame("Frame", self:GetName() .. "Panel", self, "GooeyFrame")
+	content:SetScript("OnMouseDown", function(self) self:GetParent():StartMoving() end)
+	content:SetScript("OnMouseUp", function(self) self:GetParent():StopMovingOrSizing() end)
+	content:EnableMouse(true)
+	self.content = content
 
-	local tab = CreateFrame("Button", "SageOptionsMenu" .. name, SageOptionsMenu, "SageOptionsTabButton")
-	tab:SetScript("OnClick", function() SOptions_SwitchTab(name) end)
-	tab:SetText(title)
+	--title text
+	local text = content:CreateFontString(nil, "OVERLAY")
+	text:SetPoint("TOP", content, "TOP", 0, -10)
+	text:SetFontObject("GameFontHighlightLarge")
+	text:SetText("Sage Options")
 
-	local menu = CreateFrame("Frame", "SageOptionsPanel" .. name, SageOptionsPanel, "SageOptionsPanel")
-	menu:SetScript("OnShow", OnShow)
-	menu.notFinished = 1
+	--close button
+	local close = CreateFrame("Button", nil, content, "UIPanelCloseButton")
+	close:SetPoint("TOPRIGHT", content, "TOPRIGHT", -2, -2)
+	close:SetScript("OnClick", function() HideUIPanel(self) end)
 
-	UpdateTabs()
+	self.content = content
+	return content
+end
 
+--general options
+function SageOptions:AddGeneralPanel()
+	local panel = self:AddPanel(L.General)
+
+	local function Lock_OnShow(self) self:SetChecked(Sage:IsLocked()) end
+	local function Lock_OnClick(self) Sage:SetLock(self:GetChecked()) end
+	panel:AddCheckButton(L.SetLock, Lock_OnClick, Lock_OnShow)
+
+	local function Sticky_OnShow(self) self:SetChecked(Sage:IsSticky()) end
+	local function Sticky_OnClick(self) Sage:SetSticky(self:GetChecked()) end
+	panel:AddCheckButton(L.SetSticky, Sticky_OnClick, Sticky_OnShow)
+	
+	local function OutlineBar_OnShow(self) self:SetChecked(Sage:OutlineBarFonts()) end
+	local function OutlineBar_OnClick(self) Sage:SetOutlineBarFonts(self:GetChecked()) end
+	panel:AddCheckButton(L.OutlineBarText, OutlineBar_OnClick, OutlineBar_OnShow)
+	
+	local function OutlineOut_OnShow(self) self:SetChecked(Sage:OutlineOutsideFonts()) end
+	local function OutlineOut_OnClick(self) Sage:SetOutlineOutsideFonts(self:GetChecked()) end
+	panel:AddCheckButton(L.OutlineOutside, OutlineOut_OnClick, OutlineOut_OnShow)
+	
+	local function DebuffColoring_OnShow(self) self:SetChecked(Sage:DebuffColoring()) end
+	local function DebuffColoring_OnClick(self) Sage:SetDebuffColoring(self:GetChecked()) end
+	panel:AddCheckButton(L.HealthBarDebuffColoring, DebuffColoring_OnClick, DebuffColoring_OnShow)
+	
+	local function Percents_OnShow(self) self:SetChecked(Sage:ShowingPercents()) end
+	local function Percents_OnClick(self) Sage:SetShowPercents(self:GetChecked()) end
+	panel:AddCheckButton(L.ShowHealthPercents, Percents_OnClick, Percents_OnShow)
+	
+	local function Text_OnShow(self) self:SetChecked(Sage:ShowingText()) end
+	local function Text_OnClick(self) Sage:SetShowText(self:GetChecked()) end
+	panel:AddCheckButton(L.ShowStatusBarText, Text_OnClick, Text_OnShow)
+	
+	local function CastBars_OnShow(self) self:SetChecked(Sage:ShowingCastBars()) end
+	local function CastBars_OnClick(self) Sage:SetShowCastBars(self:GetChecked()) end
+	panel:AddCheckButton(L.ShowCastBars, CastBars_OnClick, CastBars_OnShow)
+end
+
+
+--[[ Tabbed Menu ]]--
+
+local function MenuItem_OnClick(self)
+	self:GetParent():GetParent():ShowPanel(self:GetText())
+end
+
+local function Menu_AddItem(self, name)
+	local button = CreateFrame("Button", self:GetName() .. name, self)
+	button:SetWidth(52); button:SetHeight(24)
+	button:SetScript("OnClick", MenuItem_OnClick)
+
+	button:SetTextFontObject("GameFontNormalSmall")
+	button:SetHighlightTextColor(1, 1, 1)
+	button:SetText(name)
+
+	if not self.button then
+		button:SetPoint("TOP", self, "TOP", 0, -2)
+		self:SetHeight(self:GetHeight() + 30)
+	else
+		button:SetPoint("TOP", self.button, "BOTTOM")
+		self:SetHeight(self:GetHeight() + 24)
+	end
+	self.button = button
+end
+
+function SageOptions:AddMenu()
+	local menu = CreateFrame("Frame", self:GetName() .. "Menu", self, "GooeyFrame")
+	menu:SetWidth(64)
+	menu.AddItem = Menu_AddItem
+
+	self.menu = menu
 	return menu
 end
+
+function SageOptions:ShowPanel(name)
+	local menuName = self.menu:GetName()
+	for index, panel in pairs(self.panels) do
+		if(index == name) then
+			if(not panel:IsShown()) then
+				UIFrameFadeIn(panel, 0.2)
+				getglobal(menuName .. index):LockHighlight()
+				self:SetWidth(self.menu:GetWidth() + panel.width)
+				self:SetHeight(max(panel.height, self.menu:GetHeight() + 64))
+			end
+		elseif(panel:IsShown()) then
+			panel:Hide()
+			getglobal(menuName .. index):UnlockHighlight()
+		end
+	end
+end
+
+
+--[[  Panels ]]--
+
+local Panel = CreateFrame("Frame")
+local Panel_MT = {__index = Panel}
+
+function SageOptions:AddPanel(name, unit)
+	local content = self.content
+	local panel = setmetatable(CreateFrame("Frame", content:GetName() .. name, content), Panel_MT)
+	panel:SetPoint("TOPLEFT", content, "TOPLEFT", 0, -28)
+	panel:SetPoint("BOTTOMRIGHT", content, "BOTTOMRIGHT")
+	panel:Hide()
+
+	panel.width = 210
+	panel.height = 38
+
+	if(not self.panels) then self.panels = {} end
+	self.panels[name] = panel
+	self.menu:AddItem(name)
+	
+	if(unit) then
+		local function Lock_OnShow(self) self:SetChecked(Sage:IsLocked()) end
+		local function Lock_OnClick(self) Sage:SetLock(self:GetChecked()) end
+		panel:AddCheckButton(L.ShowCurable, Lock_OnClick, Lock_OnShow)
+	end
+
+	return panel
+end
+
+--slider
+function Panel:AddSlider(name)
+	local slider = CreateFrame("Slider", self:GetName() .. name, self, "GooeySlider")
+	slider:SetWidth(200); slider:SetHeight(18)
+
+	if(self.slider) then
+		slider:SetPoint("BOTTOM", self.slider, "TOP", 0, 24)
+	else
+		slider:SetPoint("BOTTOM", self, "BOTTOM", 0, 24)
+	end
+	self.height = self.height + 43
+	self.slider = slider
+
+	return slider
+end
+
+--checkbutton
+function Panel:AddCheckButton(name, OnClick, OnShow)
+	local button = CreateFrame("CheckButton", self:GetName() .. name, self, "GooeyCheckButton")
+	if(self.button) then
+		button:SetPoint("TOP", self.button, "BOTTOM", 0, 2)
+	else
+		button:SetPoint("TOPLEFT", self, "TOPLEFT", 6, 0)
+	end
+	button:SetText(name)
+	button:SetScript("OnClick", OnClick)
+	button:SetScript("OnShow", OnShow)
+
+	self.height = self.height + 30
+	self.button = button
+
+	return button
+end
+
+SageOptions:Load()
---
+++ trunk/Sage_Options/profiles.lua
@@ -0,0 +1,192 @@
+--profile options
+local listSize = 12
+local size = 19
+
+
+--[[ Profile Button ]]--
+
+local function ProfileButton_OnClick(self)
+	local parent = self:GetParent()
+	if(parent.selected) then
+		parent.selected:UnlockHighlight()
+	end
+	self:LockHighlight()
+	parent.selected = self
+end
+
+local function ProfileButton_OnMouseWheel(self, direction)
+	local scrollBar = getglobal(self:GetParent().scrollFrame:GetName() .. "ScrollBar")
+
+	scrollBar:SetValue(scrollBar:GetValue() - direction * (scrollBar:GetHeight()/2))
+	parent:UpdateList()
+	parent:Highlight()
+end
+
+local function ProfileButton_Create(name, parent)
+	local button = CreateFrame("Button", name, parent)
+	button:SetScript("OnClick", ProfileButton_OnClick)
+	button:SetScript("OnMouseWheel", ProfileButton_OnMouseWheel)
+	button:SetTextFontObject("GameFontNormal")
+
+	local highlight = button:CreateTexture()
+	highlight:SetAllPoints(button)
+	highlight:SetTexture("Interface\\QuestFrame\\UI-QuestTitleHighlight")
+	button:SetHighlightTexture(highlight)
+
+	return button
+end
+
+
+--[[ Panel Functions ]]--
+
+local function Panel_UpdateList(self)
+	local list = Sage.db:GetProfiles()
+	local size = #list
+	table.sort(list)
+
+	local scrollFrame = self.scrollFrame
+	local offset = scrollFrame.offset
+	FauxScrollFrame_Update(scrollFrame, size, listSize, listSize)
+
+	for i = 1, listSize do
+		local index = i + offset
+		local button = self.buttons[i]
+
+		if index <= size then
+			button:SetText(list[index])
+			button:Show()
+		else
+			button:Hide()
+		end
+	end
+end
+
+local function Panel_Highlight(self, profile)
+	profile = profile or Sage.db:GetCurrentProfile()
+
+	for _,button in pairs(self.buttons) do
+		if(button:GetText() == profile) then
+			button:SetTextColor(0.2, 1, 0.2)
+			button:SetHighlightTextColor(0.2, 1, 0.2)
+		else
+			button:SetTextColor(1, 0.82, 0)
+			button:SetHighlightTextColor(1, 1, 1)
+		end
+	end
+end
+
+local function Panel_Create()
+	local panel = SageOptions:AddPanel("Profiles")
+	panel.UpdateList = Panel_UpdateList
+	panel.Highlight = Panel_Highlight
+
+	local name = panel:GetName()
+
+	panel:SetScript("OnShow", function(self) panel:UpdateList(); panel:Highlight() end)
+
+	local scroll = CreateFrame("ScrollFrame", name .. "ScrollFrame", panel, "FauxScrollFrameTemplate")
+	scroll:SetScript("OnVerticalScroll", function() FauxScrollFrame_OnVerticalScroll(10, ScrollBar_Update) end)
+	scroll:SetScript("OnShow", function(self)
+		panel.buttons[1]:SetPoint("BOTTOMRIGHT", scroll, "TOPLEFT", -24, -20)
+	end)
+	scroll:SetScript("OnHide", function()
+		panel.buttons[1]:SetPoint("BOTTOMRIGHT", scroll, "TOPLEFT", -2, -20)
+	end)
+	scroll:SetPoint("TOPLEFT", panel, "TOPRIGHT", -4, -2)
+	scroll:SetPoint("BOTTOMRIGHT", panel, "BOTTOMRIGHT", -26, 42)
+	panel.scrollFrame = scroll
+
+	local set = CreateFrame("Button", name .. "Set", panel, "GooeyButton")
+	set:SetScript("OnClick", function()
+		local selected = panel.selected
+		if selected then
+			Sage:SetProfile(selected:GetText())
+			panel:UpdateList()
+			panel:Highlight(selected:GetText())
+		end
+	end)
+	set:SetText("Set"); set:SetWidth(42); set:SetHeight(24)
+	set:SetPoint("BOTTOMLEFT", panel, "BOTTOMLEFT", 4, 4)
+
+	local save = CreateFrame("Button", name .. "Save", panel, "GooeyButton")
+	save:SetScript("OnClick", function() StaticPopup_Show("SAGE_OPTIONS_SAVE_PROFILE") end)
+	save:SetText("Save"); save:SetWidth(42); save:SetHeight(24)
+	save:SetPoint("LEFT", set, "RIGHT", 4, 0)
+
+	local copy = CreateFrame("Button", name .. "Copy", panel, "GooeyButton")
+	copy:SetScript("OnClick", function()
+		local selected = panel.selected
+		if selected then
+			Sage:CopyProfile(selected:GetText())
+		end
+	end)
+	copy:SetText("Copy"); copy:SetWidth(42); copy:SetHeight(24)
+	copy:SetPoint("LEFT", save, "RIGHT", 4, 0)
+
+	local delete = CreateFrame("Button", name .. "Delete", panel, "GooeyButton")
+	delete:SetScript("OnClick", function()
+		local selected = panel.selected
+		if selected then
+			Sage:DeleteProfile(selected:GetText())
+			panel:UpdateList()
+			panel:Highlight()
+		end
+	end)
+	delete:SetText("Delete"); delete:SetWidth(42); delete:SetHeight(24)
+	delete:SetPoint("LEFT", copy, "RIGHT", 4, 0)
+
+	--add list buttons
+	panel.buttons = {}
+	for i = 1, listSize do
+		local button = ProfileButton_Create(name .. i, panel)
+		if(i == 1) then
+			button:SetPoint("TOPLEFT", panel, "TOPLEFT", 6, -4)
+			button:SetPoint("BOTTOMRIGHT", scroll, "TOPLEFT", -2, -20)
+		else
+			button:SetPoint("TOPLEFT", name .. i-1, "BOTTOMLEFT", 0, -1)
+			button:SetPoint("BOTTOMRIGHT", name .. i-1, "BOTTOMRIGHT", 0, -size)
+		end
+		panel.buttons[i] = button
+	end
+
+	panel.height = panel.height + 24 + size * listSize
+	return panel
+end
+
+StaticPopupDialogs["SAGE_OPTIONS_SAVE_PROFILE"] = {
+	text = TEXT("Enter Profile Name"),
+	button1 = TEXT(ACCEPT),
+	button2 = TEXT(CANCEL),
+	hasEditBox = 1,
+	maxLetters = 24,
+	OnAccept = function()
+		local text = getglobal(this:GetParent():GetName().."EditBox"):GetText()
+		if text ~= "" then
+			Sage:SaveProfile(text)
+			UpdateScrollBar()
+			HighlightProfile(text)
+		end
+	end,
+	EditBoxOnEnterPressed = function()
+		local text = getglobal(this:GetParent():GetName().."EditBox"):GetText()
+		if text ~= "" then
+			Sage:SaveProfile(text)
+			UpdateScrollBar()
+			HighlightProfile(text)
+		end
+	end,
+	OnShow = function()
+		getglobal(this:GetName().."EditBox"):SetFocus()
+		getglobal(this:GetName().."EditBox"):SetText(UnitName("player"))
+		getglobal(this:GetName().."EditBox"):HighlightText()
+	end,
+	OnHide = function()
+		if ChatFrameEditBox:IsVisible() then
+			ChatFrameEditBox:SetFocus()
+		end
+		getglobal(this:GetName().."EditBox"):SetText("")
+	end,
+	timeout = 0, exclusive = 1, hideOnEscape = 1
+}
+
+Panel_Create()
