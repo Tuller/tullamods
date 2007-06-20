cf82cec91676890a717251e093c43c02bfabc055
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-06-20 15:42:51 +0000

    Removed SharedMedia because it decided that it knew better than I did about what texture I wanted to use.

--- trunk/Sage/Sage.lua
+++ trunk/Sage/Sage.lua
@@ -371,13 +371,13 @@ function Sage:GetBarTexture()
 	local texID = self.profile.barTexture or "blizz"
 	if(texID:lower() == "blizz") then return BLIZZ_TEXTURE end
 
-	local SML = AceLibrary and AceLibrary:HasInstance("SharedMedia-1.0") and AceLibrary("SharedMedia-1.0")
-	if(SML) then
-		local texture = SML:Fetch(SML.MediaType.STATUSBAR, texID, true)
-		if(texture) then
-			return texture
-		end
-	end
+	-- local SML = AceLibrary and AceLibrary:HasInstance("SharedMedia-1.0") and AceLibrary("SharedMedia-1.0")
+	-- if(SML) then
+		-- local texture = SML:Fetch(SML.MediaType.STATUSBAR, texID, true)
+		-- if(texture) then
+			-- return texture
+		-- end
+	-- end
 	return format(TEXTURE_PATH, texID)
 end
 
