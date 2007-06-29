64779cc2d7f2b01c72321bce7ba3a3a779857327
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-06-29 21:14:13 +0000

    mem removal + code updates

--- trunk/Bongos2_Stats/bar.lua
+++ trunk/Bongos2_Stats/bar.lua
@@ -9,42 +9,70 @@ local L = BONGOS_STATS_LOCALS
 local UPDATE_DELAY = 1
 
 --display frame
+local function Stats_Update(self)
+	local parent = self:GetParent()
+	local sets = parent.sets
+
+	if sets.showFPS then
+		self.fps:SetText(format("%.1ffps", GetFramerate()))
+	end
+
+	if sets.showMemory then
+		self.mem:SetText(format("%.3fmb", gcinfo() / 1024))
+	end
+
+	if sets.showPing then
+		local latency = select(3, GetNetStats())
+		if (latency > PERFORMANCEBAR_MEDIUM_LATENCY) then
+			self.ping:SetTextColor(1, 0, 0)
+		elseif (latency > PERFORMANCEBAR_LOW_LATENCY) then
+			self.ping:SetTextColor(1, 1, 0)
+		else
+			self.ping:SetTextColor(0, 1, 0)
+		end
+		self.ping:SetText(format("%dms", latency))
+	end
+
+	local width = self.fps:GetStringWidth() + self.mem:GetStringWidth() + self.ping:GetStringWidth()
+	parent:SetWidth(max(24, width+4))
+end
+
 local function Stats_OnUpdate(self, arg1)
 	if not self.toNextUpdate or self.toNextUpdate <= 0 then
 		self.toNextUpdate = UPDATE_DELAY
-		BongosStats:Update()
+		Stats_Update(self)
 	else
 		self.toNextUpdate = self.toNextUpdate - arg1
 	end
 end
 
 local function Stats_OnClick(self, button)
-	if IsAltKeyDown() then
-		ReloadUI()
-	end
+	if IsAltKeyDown() then ReloadUI() end
 end
 
-local function Stats_Create(self)
-	local frame = CreateFrame("Button", nil, UIParent)
+local function Stats_Create(parent)
+	local frame = CreateFrame("Button", nil, parent)
 	frame:RegisterForClicks("AnyUp")
+	frame:SetAllPoints(parent)
 
-	self.fps = frame:CreateFontString()
-	self.fps:SetFontObject("GameFontNormalLarge")
-	self.fps:SetPoint("LEFT", frame)
+	frame.fps = frame:CreateFontString()
+	frame.fps:SetFontObject("GameFontNormalLarge")
+	frame.fps:SetPoint("LEFT", frame)
 
-	self.mem = frame:CreateFontString()
-	self.mem:SetFontObject("GameFontHighlightLarge")
-	self.mem:SetPoint("LEFT", self.fps, "RIGHT", 2, 0)
+	frame.mem = frame:CreateFontString()
+	frame.mem:SetFontObject("GameFontHighlightLarge")
+	frame.mem:SetPoint("LEFT", frame.fps, "RIGHT", 2, 0)
 
-	self.ping = frame:CreateFontString()
-	self.ping:SetFontObject("GameFontHighlightLarge")
-	self.ping:SetPoint("LEFT", self.mem, "RIGHT", 2, 0)
+	frame.ping = frame:CreateFontString()
+	frame.ping:SetFontObject("GameFontHighlightLarge")
+	frame.ping:SetPoint("LEFT", frame.mem, "RIGHT", 2, 0)
 
 	frame:SetScript("OnUpdate", Stats_OnUpdate)
 	frame:SetScript("OnClick", Stats_OnClick)
 
-	self.toNextUpdate = 0
-	self.frame = frame
+	frame.toNextUpdate = 0
+	
+	return frame
 end
 
 --stats bar
@@ -68,61 +96,21 @@ end
 
 local function Bar_OnCreate(self)
 	self.CreateMenu = Bar_CreateMenu
+	self.frame = Stats_Create(self)
 	self:SetSize(24)
 end
 
 
 --[[ Startup ]]--
 
-function BongosStats:Initialize()
-	Stats_Create(self)
-end
-
 function BongosStats:Load()
 	self.bar = BBar:Create("stats", Bar_OnCreate, nil, {
 		["y"] = 895, ["x"] = 1254, ["showMemory"] = true, ["showPing"] = true, ["showFPS"] = true,
 	})
-
-	self.bar:Attach(self.frame)
-	self.frame:SetAllPoints(self.bar)
 end
 
 function BongosStats:Unload()
 	self.bar:Destroy()
-end
-
-
---[[ Update Functions ]]--
-
-function BongosStats:Update()
-	local sets = self.bar.sets
-
-	if sets.showFPS then
-		self.fps:SetText(format("%.1ffps", GetFramerate()))
-	end
-
-	if sets.showMemory then
-		self.mem:SetText(format("%.3fmb", gcinfo() / 1024))
-	end
-
-	if sets.showPing then
-		local latency = select(3, GetNetStats())
-		if (latency > PERFORMANCEBAR_MEDIUM_LATENCY) then
-			self.ping:SetTextColor(1, 0, 0)
-		elseif (latency > PERFORMANCEBAR_LOW_LATENCY) then
-			self.ping:SetTextColor(1, 1, 0)
-		else
-			self.ping:SetTextColor(0, 1, 0)
-		end
-		self.ping:SetText(format("%dms", latency))
-	end
-
-	self:UpdateWidth()
-end
-
-function BongosStats:UpdateWidth()
-	local width = self.fps:GetStringWidth() + self.mem:GetStringWidth() + self.ping:GetStringWidth()
-	self.bar:SetWidth(max(24, width+4))
 end
 
 
@@ -131,7 +119,7 @@ end
 function BongosStats:SetShowFPS(enable)
 	if enable then
 		self.bar.sets.showFPS = true
-		self:Update()
+		Stats_Update(self.bar.frame)
 	else
 		self.bar.sets.showFPS = nil
 		self.fps:SetText("")
@@ -141,7 +129,7 @@ end
 function BongosStats:SetShowPing(enable)
 	if enable then
 		self.bar.sets.showPing = true
-		self:Update()
+		Stats_Update(self.bar.frame)
 	else
 		self.bar.sets.showPing = nil
 		self.ping:SetText("")
@@ -151,7 +139,7 @@ end
 function BongosStats:SetShowMemory(enable)
 	if enable then
 		self.bar.sets.showMemory = true
-		self:Update()
+		Stats_Update(self.bar.frame)
 	else
 		self.bar.sets.showMemory = nil
 		self.mem:SetText("")
