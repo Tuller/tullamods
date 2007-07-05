9a9f0ca301dafab5e7db5655bdbdfefab6a2c135
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-07-05 00:59:32 +0000

    Added in a clock

--- trunk/Bongos2_MapBar/bar.lua
+++ trunk/Bongos2_MapBar/bar.lua
@@ -9,6 +9,7 @@ L.ShowMapTitle = "Show Title"
 L.ShowMapZoom = "Show Zoom Buttons"
 L.ShowMapTime = "Show Day/Night Indicator"
 L.ShowMapButton = "Show Map Button"
+L.ShowClock = "Show Time"
 
 --[[
 	Compatibility Fixes
@@ -26,8 +27,67 @@ L.ShowMapButton = "Show Map Button"
 	-- TitanMovableData["MinimapCluster"] = nil
 -- end
 
+--[[ Clock Functions ]]--
+
+local offset = 0
+local UPDATE_DELAY = 30
+
+local function Clock_UpdateTime(text, hours, minutes)
+	local pm = hours >= 12
+	hours = mod(hours, 12)
+	if(hours == 0) then hours = 12 end
+
+	if pm then
+		text:SetText(format(TEXT(TIME_TWELVEHOURPM), hours, minutes))
+	else
+		text:SetText(format(TEXT(TIME_TWELVEHOURAM), hours, minutes))
+	end
+end
+
+local function Clock_OnUpdate(self, elapsed)
+	if self.elapsed <= 0 then
+		self.elapsed = UPDATE_DELAY
+
+		local hours, minutes = GetGameTime()
+		Clock_UpdateTime(self.text, hours, minutes)
+	else
+		self.elapsed = self.elapsed - elapsed
+	end
+end
+
+local function Clock_Create(parent)
+	local clock = CreateFrame("Frame", nil, parent)
+	clock:SetWidth(70) clock:SetHeight(24)
+
+	local text = clock:CreateFontString(nil, "OVERLAY")
+	text:SetFontObject("GameFontNormalSmall")
+	text:SetAllPoints(clock)
+	clock.text = text
+
+	clock.elapsed = 0
+	clock:SetScript("OnUpdate", Clock_OnUpdate)
+	clock:SetPoint("BOTTOM", parent)
+
+	return clock
+end
+
 
 --[[ Config ]]--
+
+local function Bar_ToggleClock(self, enable)
+	if enable then
+		if not self.clock then
+			self.clock = Clock_Create(self)
+		end
+		self.clock:Show()
+		self.sets.showClock = 1
+	else
+		if self.clock then
+			self.clock:Hide()
+		end
+		self.sets.showClock = nil
+	end
+end
 
 local function Bar_ToggleTitle(self, enable)
 	if enable then
@@ -102,11 +162,15 @@ local function Bar_CreateMenu(self)
 	local showDayNight = panel:AddCheckButton(L.ShowMapTime)
 	showDayNight:SetScript("OnShow", function(b) b:SetChecked(self.sets.showDay) end)
 	showDayNight:SetScript("OnClick", function(b) self:ToggleDayIndicator(b:GetChecked()) end)
-	
+
 	local showMap = panel:AddCheckButton(L.ShowMapButton)
 	showMap:SetScript("OnShow", function(b) b:SetChecked(self.sets.showMap) end)
 	showMap:SetScript("OnClick", function(b) self:ToggleMap(b:GetChecked()) end)
 	
+	local showClock = panel:AddCheckButton(L.ShowClock)
+	showClock:SetScript("OnShow", function(b) b:SetChecked(self.sets.showClock) end)
+	showClock:SetScript("OnClick", function(b) self:ToggleClock(b:GetChecked()) end)
+
 	return menu
 end
 
@@ -119,11 +183,12 @@ local function Bar_OnCreate(self)
 	self.ToggleZoom = Bar_ToggleZoomButtons
 	self.ToggleMap = Bar_ToggleMap
 	self.ToggleDayIndicator = Bar_ToggleDayIndicator
-	
+	self.ToggleClock = Bar_ToggleClock
+
 	self:EnableMouse(false)
 	self:Attach(MinimapCluster)
 	self:SetWidth(MinimapCluster:GetWidth())
-	
+
 	--make the minimap scrollable via the mousewheel
 	local scrollFrame = CreateFrame("Frame", nil, Minimap)
 	scrollFrame:SetAllPoints(Minimap)
@@ -136,19 +201,38 @@ local function Bar_OnCreate(self)
 	end)
 end
 
+-- local function Durability_OnCreate(self)
+	-- local watcher = QuestWatchFrame
+	-- if(not self:IsUserPlaced()) then
+		-- self:SetPoint("TOPRIGHT", watcher)
+	-- end
+
+-- end
+
+-- local function QuestWatch_OnCreate()
+-- end
+
 function BongosMapBar:Load()
-	local bar = BBar:Create("minimap", Bar_OnCreate, nil, nil, "BACKGROUND")
+	local defaults = {
+		showTitle = true,
+		showZoom = true,
+		showDay = true,
+		showMap = true,
+	}
+
+	local bar = BBar:Create("minimap", Bar_OnCreate, nil, defaults, "BACKGROUND")
 	if not bar:IsUserPlaced() then
 		bar:SetPoint("TOPRIGHT", UIParent)
 	end
-	
+
 	--load settings
 	--toggle title actually places the minimap on the bar, and adjusts the bar"s height
+	bar:ToggleClock(bar.sets.showClock)
 	bar:ToggleTitle(bar.sets.showTitle)
 	bar:ToggleZoom(bar.sets.showZoom)
 	bar:ToggleDayIndicator(bar.sets.showDay)
 	bar:ToggleMap(bar.sets.showMap)
-	
+
 	self.bar = bar
 end
 
