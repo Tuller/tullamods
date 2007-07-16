2e0293b4fa919fc9254c66c9b6132c65eb1f2bed
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-07-16 23:36:00 +0000

    util cleanup

--- trunk/Bagnon/core/utility.lua
+++ trunk/Bagnon/core/utility.lua
@@ -4,56 +4,54 @@
 --]]
 
 BagnonUtil = {}
-local L = BAGNON_LOCALS
+
 
 --[[ Usable Functions ]]--
 
-local currentPlayer = UnitName('player')
+local currentPlayer = UnitName("player")
+local typeContainer = select(3, GetAuctionItemClasses())
+local typeQuiver = select(7, GetAuctionItemClasses())
+local subTypeBag = select(1, GetAuctionItemSubClasses(3))
+local subTypeSoulBag = select(2, GetAuctionItemSubClasses(3))
+
 
 --[[ Bank ]]--
+
+function BagnonUtil:SetAtBank(atBank)
+	self.atBank = atBank or nil
+end
 
 function BagnonUtil:AtBank()
 	return Bagnon.atBank
 end
 
+
 --[[ Item/Bag Info Retrieval ]]--
 
 function BagnonUtil:GetInvSlot(bag)
-	if bag > 0 then
-		return ContainerIDToInventoryID(bag)
-	end
+	return bag > 0 and ContainerIDToInventoryID(bag)
 end
 
 function BagnonUtil:GetBagSize(bag, player)
 	if self:IsCachedBag(bag, player) then
-		if BagnonDB then
-			return BagnonDB:GetBagData(bag, player) or 0
-		end
+		return (BagnonDB and BagnonDB:GetBagData(bag, player)) or 0
 	else
-		if bag == KEYRING_CONTAINER then
-			return GetKeyRingSize()
-		else
-			return GetContainerNumSlots(bag)
-		end
+		return (bag == KEYRING_CONTAINER and GetKeyRingSize()) or GetContainerNumSlots(bag)
 	end
 	return 0
 end
 
 function BagnonUtil:GetBagLink(bag, player)
 	if self:IsCachedBag(bag, player) then
-		if BagnonDB then
-			return select(2, BagnonDB:GetBagData(bag, player))
-		end
+		return BagnonDB and (select(2, BagnonDB:GetBagData(bag, player)))
 	else
-		return GetInventoryItemLink('player', self:GetInvSlot(bag))
+		return GetInventoryItemLink("player", self:GetInvSlot(bag))
 	end
 end
 
 function BagnonUtil:GetItemLink(bag, slot, player)
 	if self:IsCachedBag(bag, player) then
-		if BagnonDB then
-			return (BagnonDB:GetItemData(bag, slot, player))
-		end
+		return BagnonDB and (BagnonDB:GetItemData(bag, slot, player))
 	else
 		return GetContainerItemLink(bag, slot)
 	end
@@ -97,7 +95,7 @@ function BagnonUtil:IsAmmoBag(bag, player)
 	local link = self:GetBagLink(bag, player)
 	if link then
 		local type, subType = select(6, GetItemInfo(link))
-		return (type == L.TypeQuiver or subType == L.SubTypeSoulBag)
+		return (type == typeQuiver or subType == subTypeSoulBag)
 	end
 end
 
@@ -109,7 +107,7 @@ function BagnonUtil:IsProfessionBag(bag, player)
 	local link = self:GetBagLink(bag, player)
 	if link then
 		local type, subType = select(6, GetItemInfo(link))
-		return type == L.TypeContainer and not(subType == L.SubTypeBag or subType == L.SubTypeSoulBag)
+		return type == typeContainer and not(subType == subTypeBag or subType == subTypeSoulBag)
 	end
 end
 
@@ -118,9 +116,9 @@ end
 
 function BagnonUtil:AnchorTooltip(frame)
 	if frame:GetRight() >= (GetScreenWidth() / 2) then
-		GameTooltip:SetOwner(frame, 'ANCHOR_LEFT')
+		GameTooltip:SetOwner(frame, "ANCHOR_LEFT")
 	else
-		GameTooltip:SetOwner(frame, 'ANCHOR_RIGHT')
+		GameTooltip:SetOwner(frame, "ANCHOR_RIGHT")
 	end
 end
 
@@ -128,38 +126,10 @@ function BagnonUtil:AnchorAtCursor(frame)
 	local x,y = GetCursorPosition()
 	local scale = UIParent:GetScale()
 
-	frame:SetPoint('TOPLEFT', UIParent, 'BOTTOMLEFT', x/scale - 32, y/scale + 32)
+	frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", x/scale - 32, y/scale + 32)
 end
 
 function BagnonUtil:Attach(frame, parent)
 	frame:SetParent(parent)
-	frame:SetAlpha(parent:GetAlpha())
 	frame:SetFrameLevel(1)
-end
-
-
---[[ Settings ]]--
-
-function BagnonUtil:GetSets()
-	return Bagnon.sets
-end
-
-function BagnonUtil:ShowingOwners()
-	return Bagnon.sets.showOwners
-end
-
-function BagnonUtil:ShowingBorders()
-	return Bagnon.sets.showBorders
-end
-
-function BagnonUtil:ReusingFrames()
-	return Bagnon.sets.reuseFrames
-end
-
-function BagnonUtil:ReplacingBags()
-	return Bagnon.sets.replaceBags
-end
-
-function BagnonUtil:ReplacingBank()
-	return Bagnon.sets.replaceBank
 end
