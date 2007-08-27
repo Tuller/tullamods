d3879c35c049286ca52a57ad652f0b669c5681e4
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-08-27 18:18:56 +0000

    made a copy

---
+++ branches/Bongos2_Options/localization.lua
@@ -0,0 +1,48 @@
+--[[
+	Bongos Options Localization file
+--]]
+
+--[[
+	English - Default Language
+		This version MUST always be loaded, as it has default values for all strings.
+--]]
+
+local L = BONGOS_LOCALS
+
+--general
+L.General = "General"
+L.SetLock = "Lock Bars"
+L.SetSticky = "Enable Sticky Bars"
+L.ShowMinimap = "Show Minimap Button"
+
+--profiles
+L.Profiles = "Profiles"
+L.Set = "Set"
+L.Save = "Save"
+L.Copy = "Copy"
+L.Delete = "Delete"
+L.EnterName = "Enter Profile Name"
+
+--actionbar settings
+L.ActionBar = "ActionBar"
+L.LockButtons = "Lock Button Positions"
+L.RightClickSelfCast = "Enable Right Click Selfcast"
+L.RangeCheck = "Enable Range Checking"
+L.ShowGrid = "Show Empty Buttons"
+L.ShowTooltips = "Show ActionButton Tooltips"
+L.ShowMacros = "Show Macro Text"
+L.ShowHotkeys = "Show Hotkeys"
+L.QuickMove = "Button Pickup Key"
+L.Ctrl = "Control"
+L.Alt = "Alt"
+L.Shift = "Shift"
+L.NumActionBars = "Action Bars"
+L.SelfCastKey = "Selfcast Key"
+
+--bindings panel
+L.Bindings = "Bindings"
+L.BindingsHelp = "Hover over a button, then press a key to set its binding.  To clear a button's current keybinding, press %s."
+
+--visibility panel
+L.Visibility = "Visibility"
+L.ActionBars = "Action Bars"
