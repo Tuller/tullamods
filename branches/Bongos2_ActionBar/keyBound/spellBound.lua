11a201ea1802067bc726d9930a335d04160d8107
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-09-19 22:30:10 +0000

    fixed a bug with keybound and 2.2

--- branches/Bongos2_ActionBar/keyBound/spellBound.lua
+++ branches/Bongos2_ActionBar/keyBound/spellBound.lua
@@ -1,37 +1,44 @@
 local CastButton = {}
 
 function CastButton:Load()
-	hooksecurefunc("SpellButton_OnEnter", function()
-		local id = SpellBook_GetSpellID(this:GetID())
+	local function SpellButton_OnEnter(self)
+		local id = SpellBook_GetSpellID(self:GetID())
 		local bookType = SpellBookFrame.bookType
 
 		if not(bookType == BOOKTYPE_PET or IsPassiveSpell(id, bookType)) then
-			KeyBound:Set(this)
+			KeyBound:Set(self)
 		end
-	end)
-
-	for i = 1, 12 do
-		local button = getglobal("SpellButton" .. i)
-		button.GetBindAction = CastButton.GetBindAction
-		button.GetActionName = CastButton.GetActionName
-		button.SetKey = CastButton.SetKey
-		button.GetHotkey = CastButton.GetHotkey
-		button.ClearBindings = CastButton.ClearBindings
-		button.GetBindings = CastButton.GetBindings
 	end
+
+	local i = 1
+	local button
+	repeat
+		button = getglobal('SpellButton' .. i)
+		if button then
+			button:HookScript('OnEnter', SpellButton_OnEnter)
+
+			button.GetBindAction = CastButton.GetBindAction
+			button.GetActionName = CastButton.GetActionName
+			button.SetKey = CastButton.SetKey
+			button.GetHotkey = CastButton.GetHotkey
+			button.ClearBindings = CastButton.ClearBindings
+			button.GetBindings = CastButton.GetBindings
+			i = i + 1
+		end
+	until not button
 end
 
 function CastButton:GetActionName()
 	local name, subName = GetSpellName(SpellBook_GetSpellID(self:GetID()), SpellBookFrame.bookType)
-	if(subName and subName ~= "") then
-		return format("%s(%s)", name, subName)
+	if(subName and subName ~= '') then
+		return format('%s(%s)', name, subName)
 	end
 	return name
 end
 
 -- returns the keybind action of the given button
 function CastButton:GetBindAction()
-	return format("SPELL %s", self:GetActionName())
+	return format('SPELL %s', self:GetActionName())
 end
 
 -- binds the given key to the given button
@@ -51,12 +58,12 @@ end
 function CastButton:GetBindings()
 	local keys
 	local binding = self:GetBindAction()
-	for i = 1, select("#", GetBindingKey(binding)) do
+	for i = 1, select('#', GetBindingKey(binding)) do
 		local hotKey = select(i, GetBindingKey(binding))
 		if keys then
-			keys = keys .. ", " .. GetBindingText(hotKey, "KEY_")
+			keys = keys .. ', ' .. GetBindingText(hotKey, 'KEY_')
 		else
-			keys = GetBindingText(hotKey, "KEY_")
+			keys = GetBindingText(hotKey, 'KEY_')
 		end
 	end
 	return keys
