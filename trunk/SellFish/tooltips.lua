20f2e68ae139604f634d8e0235ce72eac8df2b0b
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-09-27 19:07:49 +0000

    implemented some more tooltip hooks, fixed a few (thanks Polarina)

--- trunk/SellFish/tooltips.lua
+++ trunk/SellFish/tooltips.lua
@@ -27,144 +27,153 @@ local SILVER_TEXT = format("|cffc7c7cf%s|r", "s")
 local COPPER_TEXT = format("|cffeda55f%s|r", "c")
 
 local function AddMoneyToTooltip(tip, id, count)
-    if id then
-		local cost = (GetSellValue(id) or 0) * (count or 1)
-        if cost > 0 then
-			local style = SellFishDB.style
-			--blizard style
-			if(style == 1) then
-				SetTooltipMoney(tip, cost)
-			--cola light
-			elseif(style == 2) then
-				local costText
-				--yes, this code is from abacus (ckknight)
-				if cost >= 10000 then
-					costText = format("|cffffffff%.2f|r%s", cost/10000, GOLD_TEXT);
-				elseif cost >= 100 then
-					costText = format("|cffffffff%.2f|r%s", cost/100, SILVER_TEXT);
-				else
-					costText = format("|cffffffff%d|r%s", cost, COPPER_TEXT);
-				end
-				if count and count > 1 then
-					tip:AddDoubleLine(format(SELLFISH_LOCALS.SellsForMany, count), costText, 0, 1, 1, 1, 1, 1, 0)
-				else
-					tip:AddDoubleLine(SELLFISH_LOCALS.SellsFor, costText, 0, 1, 1, 1, 1, 1, 0)
-				end
-			--item price
-			elseif(style == 3) then
-				local gold = floor(cost / 10000)
-				local silver = mod(cost, 10000)/100
-				local copper = mod(cost, 100)
-				local text
-
-				if(gold > 0) then
-					text = format("|cffffffff%d|r%s", gold, GOLD_TEXT)
-				end
-				if(silver > 0) then
-					if(text) then
-						text = text .. format(" |cffffffff%d|r%s", silver, SILVER_TEXT)
-					else
-						text = format("|cffffffff%d|r%s", silver, SILVER_TEXT)
-					end
-				end
-				if(copper > 0) then
-					if(text) then
-						text = text .. format(" |cffffffff%d|r%s", copper, COPPER_TEXT)
-					else
-						text = format("|cffffffff%d|r%s", copper, COPPER_TEXT)
-					end
-				end
-
-				if count and count > 1 then
-					tip:AddDoubleLine(format(SELLFISH_LOCALS.SellsForMany, count), text, 0, 1, 1, 1, 1, 1, 0)
-				else
-					tip:AddDoubleLine(SELLFISH_LOCALS.SellsFor, text, 0, 1, 1, 1, 1, 1, 0)
-				end
-			--money tooltips
+	local cost = (GetSellValue(id) or 0) * (count or 1)
+	if cost > 0 then
+		local style = SellFishDB.style
+		--blizard style
+		if(style == 1) then
+			SetTooltipMoney(tip, cost)
+		--cola light
+		elseif(style == 2) then
+			local costText
+			--yes, this code is from abacus (ckknight)
+			if cost >= 10000 then
+				costText = format("|cffffffff%.2f|r%s", cost/10000, GOLD_TEXT);
+			elseif cost >= 100 then
+				costText = format("|cffffffff%.2f|r%s", cost/100, SILVER_TEXT);
 			else
-				SetTooltipMoney(tip, cost)
+				costText = format("|cffffffff%d|r%s", cost, COPPER_TEXT);
 			end
-			tip:Show()
+			if count and count > 1 then
+				tip:AddDoubleLine(format(SELLFISH_LOCALS.SellsForMany, count), costText, 0, 1, 1, 1, 1, 1, 0)
+			else
+				tip:AddDoubleLine(SELLFISH_LOCALS.SellsFor, costText, 0, 1, 1, 1, 1, 1, 0)
+			end
+		--item price
+		elseif(style == 3) then
+			local gold = floor(cost / 10000)
+			local silver = mod(cost, 10000)/100
+			local copper = mod(cost, 100)
+			local text
+
+			if(gold > 0) then
+				text = format("|cffffffff%d|r%s", gold, GOLD_TEXT)
+			end
+			if(silver > 0) then
+				if(text) then
+					text = text .. format(" |cffffffff%d|r%s", silver, SILVER_TEXT)
+				else
+					text = format("|cffffffff%d|r%s", silver, SILVER_TEXT)
+				end
+			end
+			if(copper > 0) then
+				if(text) then
+					text = text .. format(" |cffffffff%d|r%s", copper, COPPER_TEXT)
+				else
+					text = format("|cffffffff%d|r%s", copper, COPPER_TEXT)
+				end
+			end
+
+			if count and count > 1 then
+				tip:AddDoubleLine(format(SELLFISH_LOCALS.SellsForMany, count), text, 0, 1, 1, 1, 1, 1, 0)
+			else
+				tip:AddDoubleLine(SELLFISH_LOCALS.SellsFor, text, 0, 1, 1, 1, 1, 1, 0)
+			end
+		--money tooltips
+		else
+			SetTooltipMoney(tip, cost)
 		end
+		tip:Show()
 	end
 end
 
-local function hookTip(action, method)
-	return function(tip, ...)
-		local arg = action(tip, ...)
-
-		if (tip == GameTooltip or tip == ItemRefTooltip) then
-			local link, count = method(...)
+local function hookTip(tooltip, method, action)
+	hooksecurefunc(tooltip, method, function(tip, ...)
+		local link, count = action(...)
+		if link then
 			AddMoneyToTooltip(tip, link, count)
 		end
-		return arg;
-	end
+	end)
 end
 
-ItemRefTooltip.SetHyperlink = hookTip(ItemRefTooltip.SetHyperlink, function(link, count)
-	return link, count
-end)
+local hooks = {
+	SetHyperlink = function(link, count)
+		return link, count
+	end,
 
-GameTooltip.SetBagItem = hookTip(GameTooltip.SetBagItem, function(bag, slot)
-	if not MerchantFrame:IsVisible() then
-		return GetContainerItemLink(bag, slot), select(2, GetContainerItemInfo(bag, slot))
-	end
-end)
+	SetBagItem = function(bag, slot)
+		if not MerchantFrame:IsVisible() then
+			return GetContainerItemLink(bag, slot), select(2, GetContainerItemInfo(bag, slot))
+		end
+	end,
 
-GameTooltip.SetLootItem = hookTip(GameTooltip.SetLootItem, function(slot)
-	return GetLootSlotLink(slot), select(3, GetLootSlotInfo(slot))
-end)
+	SetInventoryItem = function(unit, slot)
+		return GetInventoryItemLink(unit, slot), GetInventoryItemCount(unit, slot)
+	end,
 
-GameTooltip.SetLootRollItem = hookTip(GameTooltip.SetLootRollItem, function(slot)
-	return GetLootRollItemLink(slot), select(3, GetLootRollItemInfo(slot))
-end)
+	SetLootItem = function(slot)
+		if LootSlotIsItem(slot) then
+			return GetLootSlotLink(slot), select(3, GetLootSlotInfo(slot))
+		end
+	end,
 
-GameTooltip.SetHyperlink = hookTip(GameTooltip.SetHyperlink, function(link, count)
-	return link, count
-end)
+	SetLootRollItem = function(slot)
+		return GetLootRollItemLink(slot), select(3, GetLootRollItemInfo(slot))
+	end,
 
-GameTooltip.SetAuctionItem = hookTip(GameTooltip.SetAuctionItem , function(type, index)
-	return GetAuctionItemLink(type, index), select(3, GetAuctionItemInfo(type, index))
-end)
+	SetAuctionItem = function(type, index)
+		return GetAuctionItemLink(type, index), select(3, GetAuctionItemInfo(type, index))
+	end,
 
-GameTooltip.SetQuestItem = hookTip(GameTooltip.SetQuestItem, function(type, index)
-	return GetQuestItemLink(type, index), select(3, GetQuestItemInfo(type, index))
-end)
+	SetAuctionSellItem = function()
+		return select(2, GetItemInfo(GetAuctionSellItemInfo())), select(3, GetAuctionSellItemInfo())
+	end,
 
-GameTooltip.SetCraftItem = hookTip(GameTooltip.SetCraftItem, function(skill, id)
-	if id then
-		return GetCraftReagentItemLink(skill, id), select(3, GetCraftReagentInfo(skill, id))
-	end
-	return GetCraftItemLink(skill)
-end)
+	SetCraftItem = function(skill, id)
+		if id then
+			return GetCraftReagentItemLink(skill, id), select(3, GetCraftReagentInfo(skill, id))
+		end
+		return GetCraftItemLink(skill)
+	end,
 
-GameTooltip.SetInventoryItem = hookTip(GameTooltip.SetInventoryItem , function(unit, slot)
-	return GetInventoryItemLink(unit, slot), GetInventoryItemCount(unit, slot)
-end)
+	SetTradeSkillItem = function(skill, id)
+		if id then
+			return GetTradeSkillReagentItemLink(skill, id), select(3, GetTradeSkillReagentInfo(skill, id))
+		end
+		return GetTradeSkillItemLink(skill)
+	end,
 
-GameTooltip.SetTradeSkillItem = hookTip(GameTooltip.SetTradeSkillItem, function(skill, id)
-	if id then
-		return GetTradeSkillReagentItemLink(skill, id), select(3, GetTradeSkillReagentInfo(skill, id))
-	end
-	return GetTradeSkillItemLink(skill)
-end)
+	SetQuestItem = function(type, index)
+		return GetQuestItemLink(type, index), select(3, GetQuestItemInfo(type, index))
+	end,
 
-GameTooltip.SetQuestLogItem = hookTip(GameTooltip.SetQuestLogItem, function(type, index)
-	local link = GetQuestLogItemLink(type, index)
-	if type == 'choice' then
-		return link, select(3, GetQuestLogChoiceInfo(index))
-	end
-	return link, select(3, GetQuestLogRewardInfo(index))
-end)
+	SetQuestLogItem = function(type, index)
+		local link = GetQuestLogItemLink(type, index)
+		if type == 'choice' then
+			return link, select(3, GetQuestLogChoiceInfo(index))
+		end
+		return link, select(3, GetQuestLogRewardInfo(index))
+	end,
 
-GameTooltip.SetTradePlayerItem = hookTip(GameTooltip.SetTradePlayerItem, function(id)
-	return GetTradePlayerItemLink(id), select(3, GetTradePlayerItemInfo(id))
-end)
+	SetTradePlayerItem = function(id)
+		return GetTradePlayerItemLink(id), select(3, GetTradePlayerItemInfo(id))
+	end,
 
-GameTooltip.SetTradeTargetItem = hookTip(GameTooltip.SetTradeTargetItem, function(id)
-	return GetTradeTargetItemLink(id), select(3, GetTradeTargetItemInfo(id))
-end)
+	SetTradeTargetItem = function(id)
+		return GetTradeTargetItemLink(id), select(3, GetTradeTargetItemInfo(id))
+	end,
 
-GameTooltip.SetInboxItem = hookTip(GameTooltip.SetInboxItem, function(id)
-	return GetInboxItemLink(id), select(3, GetInboxItem(id))
-end)
+	SetInboxItem = function(id)
+		return GetInboxItemLink(id), select(3, GetInboxItem(id))
+	end,
+	
+	SetSendMailItem = function()
+		return select(2, GetItemInfo(GetSendMailItem())), select(3, GetSendMailItem())
+	end,
+}
+
+hookTip(ItemRefTooltip, 'SetHyperlink', hooks.SetHyperlink)
+
+for method,func in pairs(hooks) do
+	hookTip(GameTooltip, method, func)
+end
