df9ecc517692f49fe966eca345ba7db6692b4cb5
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-10-04 04:10:45 +0000

    localization tweaks

--- trunk/OmniCC_Options/menu.lua
+++ trunk/OmniCC_Options/menu.lua
@@ -3,23 +3,8 @@
 		A configuration GUI for OmniCC
 --]]
 
-local L = OMNICC_LOCALS
-L.Thin = 'Thin'
-L.Thick = 'Thick'
-L.ShowModels = 'Show Cooldown Models'
-L.ShowPulse = 'Show Pulse'
-L.UseMMSS = 'Use MM:SS Format'
-L.MinDuration = 'Minimum Duration'
-L.MinScale = 'Minimum Scale'
-L.Display = 'Display'
-L.FontFace = 'Font Face'
-L.FontOutline = 'Font Outline'
-L.FontSize = 'Font Size'
-L.Font = 'Font'
-L.ColorsAndScaling = 'Colors and Scaling'
-L.UnderFiveSeconds = 'Under 5s'
-
 local SML = LibStub and LibStub:GetLibrary('LibSharedMedia-2.0') --shared media library
+local L = OMNICC_LOCALS
 
 OmniCCOptions = {}
 
