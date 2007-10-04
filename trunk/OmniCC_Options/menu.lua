e4c0e2301ad627344ad8578b8ce06d304dbf3416
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-10-04 03:22:36 +0000

    OmniCC Options: a load on demand options window for OmniCC Initial import

---
+++ trunk/OmniCC_Options/menu.lua
@@ -0,0 +1,114 @@
+--[[
+	OmiCC Options
+		A configuration GUI for OmniCC
+--]]
+
+local L = OMNICC_LOCALS
+L.Thin = 'Thin'
+L.Thick = 'Thick'
+L.ShowModels = 'Show Cooldown Models'
+L.ShowPulse = 'Show Pulse'
+L.UseMMSS = 'Use MM:SS Format'
+L.MinDuration = 'Minimum Duration'
+L.MinScale = 'Minimum Scale'
+L.Display = 'Display'
+L.FontFace = 'Font Face'
+L.FontOutline = 'Font Outline'
+L.FontSize = 'Font Size'
+L.Font = 'Font'
+L.ColorsAndScaling = 'Colors and Scaling'
+L.UnderFiveSeconds = 'Under 5s'
+
+local SML = LibStub and LibStub:GetLibrary('LibSharedMedia-2.0') --shared media library
+
+OmniCCOptions = {}
+
+function OmniCCOptions.LoadColorPicker(frame)
+	local function SetColor(r, g, b)
+		frame:GetNormalTexture():SetVertexColor(r, g, b)
+		OmniCC:SetDurationColor(frame:GetParent().duration, r, g, b)
+	end
+
+	local function OnColorChange()
+		SetColor(ColorPickerFrame:GetColorRGB())
+	end
+
+	local function OnCancelChanges()
+		SetColor(frame.r, frame.g, frame.b)
+	end
+
+	frame:SetScript('OnClick', function(self)
+		if ColorPickerFrame:IsShown() then
+			ColorPickerFrame:Hide()
+		else
+			self.r, self.g, self.b = OmniCC:GetDurationFormat(self:GetParent().duration)
+			self.swatchFunc = OnColorChange
+			self.cancelFunc = OnCancelChanges
+
+			UIDropDownMenuButton_OpenColorPicker(self)
+			ColorPickerFrame:SetFrameStrata('TOOLTIP')
+			ColorPickerFrame:Raise()
+		end
+	end)
+end
+
+--[[ Dropdowns ]]--
+
+local info = {}
+local function AddItem(text, value, func, checked)
+	info.text = text
+	info.func = func
+	info.value = value
+	info.checked = checked
+	UIDropDownMenu_AddButton(info)
+end
+
+function OmniCC.OnFontFaceLoad(frame)
+	function frame.OnClick()
+		OmniCC:SetFont(this.value)
+		UIDropDownMenu_SetSelectedName(frame, this.value)
+	end
+
+	function frame.Initialize()
+		local selectedFont = OmniCC:GetFontName()
+		for _,font in ipairs(SML:List(SML.MediaType.FONT)) do
+			AddItem(font, font, frame.OnClick, font == selectedFont)
+		end
+	end
+
+	frame:SetScript('OnShow', function(self)
+		UIDropDownMenu_Initialize(self, self.Initialize)
+		UIDropDownMenu_SetWidth(132, self)
+		UIDropDownMenu_SetSelectedName(self, OmniCC:GetFontName())
+	end)
+end
+
+function OmniCC.OnFontOutlineLoad(frame)
+	local styles = {NONE, 'Thin', 'Thick'}
+	local outlines = {nil, 'OUTLINE', 'THICKOUTLINE'}
+
+	function frame.OnClick()
+		OmniCC:SetFontOutline(outlines[this.value])
+		UIDropDownMenu_SetSelectedValue(frame, this.value)
+	end
+
+	function frame.Initialize()
+		local selectedOutline = OmniCC:GetFontOutline()
+		for i,style in ipairs(styles) do
+			AddItem(style, i, frame.OnClick, outlines[i] == selectedOutline)
+		end
+	end
+
+	frame:SetScript('OnShow', function(self)
+		UIDropDownMenu_Initialize(self, self.Initialize)
+		UIDropDownMenu_SetWidth(132, self)
+
+		local selectedOutline = OmniCC:GetFontOutline()
+		for i, outline in pairs(outlines) do
+			if outline == selectedOutline then
+				UIDropDownMenu_SetSelectedValue(self, i)
+				break
+			end
+		end
+	end)
+end
