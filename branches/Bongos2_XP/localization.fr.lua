a2d2d1d0b168359a3a84a19c4f5b1f5789c4c658
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2007-11-07 18:11:12 +0000

    2.3 branch

---
+++ branches/Bongos2_XP/localization.fr.lua
@@ -0,0 +1,12 @@
+--[[
+	Bongos XP Localization file
+--]]
+
+if GetLocale() ~= 'frFR' then return end
+
+local L = BONGOS_LOCALS
+L.Height = "Hauteur"
+L.Width = "Largeur"
+L.Vertical = "Vertical"
+L.AlwaysShowText = "Toujours afficher le texte"
+L.TextPosition = "Position du texte"
