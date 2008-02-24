eccee069e166bff39b258594b8f3d7a01db25645
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-02-24 18:31:03 +0000

    removed the happy frame since its no longer necessary

--- Sage/branches/Ace3/Sage_Pet/pet.lua
+++ Sage/branches/Ace3/Sage_Pet/pet.lua
@@ -9,71 +9,6 @@ local DEBUFF_SIZE = 25
 local BUFF_SIZE = 16
 
 
---[[  Templates ]]--
-
-local function HappyFrame_OnEnter(self)
-	if self.tooltip then
-		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-		GameTooltip:SetText(self.tooltip)
-		if self.tooltipDamage then
-			GameTooltip:AddLine(self.tooltipDamage, "", 1, 1, 1)
-		end
-		if self.tooltipLoyalty then
-			GameTooltip:AddLine(self.tooltipLoyalty, "", 1, 1, 1)
-		end
-		GameTooltip:Show()
-	end
-end
-
-local function HappyFrame_OnLeave() GameTooltip:Hide() end
-
-local function HappyFrame_Update(self)
-	local happiness, damagePercentage, loyaltyRate = GetPetHappiness()
-	local hasPetUI, isHunterPet = HasPetUI()
-
-	if not(happiness or isHunterPet) then
-		self:Hide()
-	else
-		self:Show()
-
-		if happiness == 1 then
-			self.icon:SetTexCoord(0.375, 0.5625, 0, 0.359375)
-		elseif happiness == 2 then
-			self.icon:SetTexCoord(0.1875, 0.375, 0, 0.359375)
-		elseif happiness == 3 then
-			self.icon:SetTexCoord(0, 0.1875, 0, 0.359375)
-		end
-
-		self.tooltip = getglobal("PET_HAPPINESS".. happiness)
-		self.tooltipDamage = format(PET_DAMAGE_PERCENTAGE, damagePercentage)
-
-		if loyaltyRate < 0 then
-			self.tooltipLoyalty = LOSING_LOYALTY
-		elseif loyaltyRate > 0 then
-			self.tooltipLoyalty = GAINING_LOYALTY
-		else
-			self.tooltipLoyalty = nil
-		end
-	end
-end
-
-local function HappyFrame_Create(parent)
-	local frame = CreateFrame("Frame", nil, parent)
-	frame:SetWidth(26); frame:SetHeight(30)
-	frame:EnableMouse(true)
-
-	frame.icon = frame:CreateTexture(nil, "BACKGROUND")
-	frame.icon:SetTexture("Interface\\PetPaperDollFrame\\UI-PetHappiness")
-	frame.icon:SetAllPoints(frame)
-	frame.Update = HappyFrame_Update
-
-	frame:SetScript("OnEnter", HappyFrame_OnEnter)
-	frame:SetScript("OnLeave", HappyFrame_OnLeave)
-
-	return frame
-end
-
-
 --[[ Update Functions ]]--
 
 local function Frame_UpdateCombatStatus(self)
@@ -84,7 +19,6 @@ end
 local function Frame_Update(self)
 	self:UpdateCombatStatus()
 
-	if self.happy then self.happy:Update() end
 	self.info:UpdateAll()
 	self.health:UpdateAll()
 	self.mana:Update()
@@ -103,20 +37,9 @@ local function Frame_OnCreate(self)
 	self.extraWidth = (self.extraWidth or 0) + debuff:GetWidth()
 	self.debuff = debuff
 
-	if select(2, UnitClass("player")) == "HUNTER" then
-		local happy = HappyFrame_Create(self)
-		happy:SetPoint("TOPRIGHT", self.debuff, "TOPLEFT", 0, 2)
-		self.extraWidth = (self.extraWidth or 0) + happy:GetWidth()
-		self.happy = happy
-	end
-
 	local info = SageInfo:Create(self)
 	info:SetPoint("TOPLEFT", self)
-	if self.happy then
-		info:SetPoint("BOTTOMRIGHT", self.happy, "TOPLEFT", 0, -2)
-	else
-		info:SetPoint("BOTTOMRIGHT", self.debuff, "TOPLEFT")
-	end
+	info:SetPoint("BOTTOMRIGHT", self.debuff, "TOPLEFT")
 	self.info = info
 
 	local health = SageHealth:Create(self)
@@ -177,9 +100,6 @@ function SagePet:Load()
 	self:RegisterEvent("UNIT_PET")
 	self:RegisterEvent("PET_ATTACK_START", "OnCombatEvent")
 	self:RegisterEvent("PET_ATTACK_STOP", "OnCombatEvent")
-	if frame.happy then
-		self:RegisterEvent("UNIT_HAPPINESS")
-	end
 
 	if(UnitExists("pet")) then
 		self.frame:Update()
@@ -194,12 +114,6 @@ end
 function SagePet:UNIT_PET(event, unit)
 	if(unit == "player") then
 		self.frame:Update()
-	end
-end
-
-function SagePet:UNIT_HAPPINESS(event, unit)
-	if(unit == "pet") then
-		self.frame.happy:Update()
 	end
 end
 
