e7302d910b4c42bfe9e94aa304b0a92d201a82f1
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-03-25 21:43:37 +0000

    Created bongos3 dir

---
+++ Bongos3/trunk/Bongos_AB/localization/localization.es.lua
@@ -0,0 +1,21 @@
+﻿--[[
+	Bongos_Actionbar Localization
+	Spanish by Ferroginu from Zul'jin
+	¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ SAVE in UTF-8 !!!!!!!!!!!!!!!!!!!!!!!!
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('Bongos3-AB', 'esES')
+if not L then return end
+
+L.Columns = 'Columnas'
+L.Size = 'Tamaño'
+L.Vertical = 'Vertical'
+L.OneBag = 'Una Bolsa'
+L.ShowKeyring = 'Mostrar Llavero'
+L.Paging = 'Paginación'
+L.Stances = 'Actitud'
+L.Page = 'Pagina %s'
+L.FriendlyTarget = 'Objetivo amistoso'
+L.Modifier = 'Modificador'
+L.Prowl = 'Rondando'
+L.ShadowForm = 'Forma de Sombra/Redención'
---
+++ Bongos3/trunk/Bongos_XP/localization/localization.lua
@@ -0,0 +1,12 @@
+--[[
+	Bongos XP Localization file
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('Bongos3-XP', 'enUS', true)
+
+L.Height = 'Height'
+L.Width = 'Width'
+L.Vertical = 'Vertical'
+L.AlwaysShowText = 'Always Show Text'
+L.AlwaysShowXP = 'Always Show Experience'
+L.TextPosition = 'Text position'
