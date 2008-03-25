bfbf698bdad044eac9f1bedb19281ad01ca2eb3d
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-03-25 21:45:18 +0000

    that's what she said

--- Bongos/branches/3.0/Bongos_CastBar/localization/localization.fr.lua
+++
@@ -1,8 +0,0 @@
-﻿--[[
-	Bongos CastBar Localization file
---]]
-
-local L = LibStub('AceLocale-3.0'):NewLocale('Bongos3-CastBar', 'frFR')
-if not L then return end
-
-L.ShowTime = 'Montrer le temps'
--- Bongos/branches/3.0/Bongos_CastBar/localization/localization.lua
+++
@@ -1,6 +0,0 @@
---[[
-	Bongos CastBar Localization file
---]]
-
-local L = LibStub('AceLocale-3.0'):NewLocale('Bongos3-CastBar', 'enUS', true)
-L.ShowTime = 'Show Time'
