693ec241b8a5e93fbd58c3a713bd680bb1f55cf0
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-05-25 19:33:22 +0000

    added cn translation

--- Bongos3/trunk/Bongos/localization/localization.cn.lua
+++ Bongos3/trunk/Bongos/localization/localization.cn.lua
@@ -1,8 +1,6 @@
-﻿--[[
+--[[
 	Bongos' Localization File
-		Chinese Simplified by ondh
-		http://www.ondh.cn
-		
+		Chinese Simplified by Jokey http://www.biggamer.com
 	UTF-8!
 --]]
 
@@ -12,7 +10,6 @@ if not L then return end
 --system messages
 L.NewPlayer = "建立新配置 %s"
 L.Updated = "升级到 v%s"
-L.UpdatedIncompatible = "无法从不兼容的版本升级. 加载默认配置"
 
 --profiles
 L.ProfileCreated = "建立新配置 \"%s\""
@@ -21,6 +18,7 @@ L.ProfileDeleted = "删除配置 \"%s\""
 L.ProfileCopied = "从 \"%s\" 复制配置到 \"%s\""
 L.ProfileReset = "重置配置 \"%s\""
 L.CantDeleteCurrentProfile = "不能删除当前配置"
+L.InvalidProfile = '无效的配置文件 "%s"'
 
 --slash command help
 L.ShowOptionsDesc = "显示设置菜单"
@@ -29,6 +27,7 @@ L.StickyBarsDesc = "动作条自动定位开关"
 
 L.SetScaleDesc = "设置缩放 <barList>"
 L.SetAlphaDesc = "设置透明度 <barList>"
+L.SetFadeDesc = '设置淡出透明度 <barList>'
 
 L.ShowBarsDesc = "显示设置 <barList>"
 L.HideBarsDesc = "隐藏设置 <barList>"
@@ -48,6 +47,7 @@ L.PrintVersionDesc = "显示当前 Bongos 版本"
 L.ShowConfig = "<右键> 设置"
 L.HideBar = "<中键或者Shift+右键> 隐藏"
 L.ShowBar = "<中键或者Shift+右键> 显示"
+L.DeleteBar = '<Alt+右键> 删除'
 L.SetAlpha = "<滚轮> 设置透明度 (|cffffffff%d|r)"
 
 --Menu Stuff
@@ -59,9 +59,20 @@ L.Spacing = "间距"
 L.Layout = "布局"
 
 --minimap button stuff
-L.ShowMenuTip = "<右键> 显示设置菜单"
-L.HideMenuTip = "<右键> 隐藏设置菜单"
-L.LockBarsTip = "<左键> 锁定动作条位置"
-L.UnlockBarsTip = "<左键> 解锁动作条位置"
-L.LockButtonsTip = "<Shift+左键> 锁定动作条按钮"
-L.UnlockButtonsTip = "<Shift+左键> 解锁动作条按钮"
+L.ConfigEnterTip = '<左键> 进入设置菜单'
+L.ConfigExitTip = '<左键> 退出设置菜单'
+L.BindingEnterTip = '<Shift+左键> 进入按键绑定菜单'
+L.BindingExitTip = '<Shift+左键> 退出按键绑定菜单'
+L.ShowOptionsTip = '<右键> 显示设置菜单'
+
+--Options Menu
+L.EnableStickyBars = '动作条吸附'
+L.ShowMinimapButton = '显示迷你地图按钮'
+L.General = '综合'
+L.Profiles = '配置文件'
+L.Visibility = '可见'
+L.Copy = '拷贝'
+L.Set = '设置'
+L.Save = '保存'
+L.Delete = '删除'
+L.EnterName = '输入配置文件名'
