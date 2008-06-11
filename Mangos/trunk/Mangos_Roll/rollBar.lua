8105f75334b14e4d7bbc3caa1a8a761e9faea5fa
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-06-11 01:05:23 +0000

    roll bar - initial import

---
+++ Mangos/trunk/Mangos_Roll/rollBar.lua
@@ -0,0 +1,52 @@
+﻿--[[
+	Mangos
+		Because sometimes I feel bad about doing to much
+--]]
+
+
+--[[ Module Stuff ]]--
+
+local MRB = Mangos:NewModule('roll')
+local RollBar
+
+function MRB:Load()
+	self.frame = RollBar:New()
+end
+
+function MRB:Free()
+	self.frame:Free()
+end
+
+
+--[[ Roll Bar Object ]]--
+
+RollBar = Mangos:CreateClass('Frame', Mangos.Frame)
+
+function RollBar:New()
+	local f = self.super.New(self, 'roll')
+	f:LoadButtons()
+	f:Layout()
+
+	return f
+end
+
+function RollBar:GetDefaults()
+	return {
+		point = 'LEFT',
+		numButtons = NUM_GROUP_LOOT_FRAMES,
+		columns = 1,
+		spacing = 2
+	}
+end
+
+function RollBar:AddButton(i)
+	local b =  getglobal('GroupLootFrame' .. i)
+	b:SetParent(self.header)
+	self.buttons[i] = b
+end
+
+function RollBar:RemoveButton(i)
+	local b = self.buttons[i]
+	b:SetParent(nil)
+	self.buttons[i] = nil
+end
