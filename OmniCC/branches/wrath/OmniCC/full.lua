472f048dd8cdb3344d5599dab38c80829ea60256
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-09-28 16:15:25 +0000

    secret anti pulse option

--- OmniCC/branches/wrath/OmniCC/full.lua
+++ OmniCC/branches/wrath/OmniCC/full.lua
@@ -113,7 +113,6 @@ do
 				HideTimer(self)
 			else
 				self:SetAlpha(OmniCC.sets.showModel and 1 or 0)
-
 				if start > 0 and duration > OmniCC.sets.minDuration then
 					OmniCC:StartTimer(self, start, duration)
 				else
@@ -138,6 +137,7 @@ function OmniCC:StartTimer(cooldown, start, duration)
 
 		timer.start = start
 		timer.duration = duration
+		timer.shouldPulse = duration > 30
 		timer.nextUpdate = 0
 		timer:Show()
 	end
@@ -240,7 +240,7 @@ function OmniCC:UpdateTimer(timer)
 		else
 			timer:Hide()
 
-			if self:ShowingPulse() then
+			if timer.shouldPulse and self:ShowingPulse() then
 				self:StartPulse(timer)
 			end
 		end
