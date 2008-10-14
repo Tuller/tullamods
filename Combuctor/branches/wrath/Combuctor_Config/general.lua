7641d0170b67bc01d999b91615fce0590388a529
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-10-14 01:30:00 +0000

    options menu complete, for now

--- Combuctor/branches/wrath/Combuctor_Config/general.lua
+++ Combuctor/branches/wrath/Combuctor_Config/general.lua
@@ -1,29 +1,36 @@
 --[[
-	A profile selector panel
+	A combuctor panel for adding and removing categories from the bank/inventory frames
+		This is a rush job, I should redo it later
 --]]
 
---local L = LibStub('AceLocale-3.0'):GetLocale('Dominos-Config')
 local CombuctorSet = Combuctor:GetModule('Sets')
+local L = LibStub("AceLocale-3.0"):GetLocale("Combuctor")
 local MAX_ITEMS = 13
-local height, offset = 24, 0
+local height, offset = 26, 0
 local selected = {}
 local items = {}
 local profile = Combuctor:GetProfile()
 local key = 'inventory'
 
-local function AddSet(sets, name)
+local function AddSet(name)
+	local info = Combuctor:GetProfile()[key]
+	local sets = info.sets
+
 	for i, set in pairs(sets) do
 		if set.name == name then
 			return
 		end
 	end
-	table.insert(sets, {['name'] = name})
+	table.insert(sets, name)
 	Combuctor:SendMessage('COMBUCTOR_CONFIG_SET_ADD', key, name)
 end
 
-local function RemoveSet(sets, name)
+local function RemoveSet(name)
+	local info = Combuctor:GetProfile()[key]
+	local sets = info.sets
+
 	for i,set in pairs(sets) do
-		if set.name == name then
+		if set == name then
 			table.remove(sets, i)
 			Combuctor:SendMessage('COMBUCTOR_CONFIG_SET_REMOVE', key, name)
 			break
@@ -31,92 +38,143 @@ local function RemoveSet(sets, name)
 	end
 end
 
-local function AddSubSet(sets, name, parent)
-	for i,set in pairs(sets) do
-		if set.name == parent then
-			if set.exclude then
-				for j,child in pairs(set.exclude) do
-					if child == name then
-						table.remove(set.exclude, j)
-						Combuctor:SendMessage('COMBUCTOR_CONFIG_SUBSET_REMOVE', key, name, parent)
-						break
-					end
+local function AddSubSet(name, parent)
+	local info = Combuctor:GetProfile()[key]
+	local exclude = info.exclude[parent]
+
+	if exclude then
+		for i,set in pairs(exclude) do
+			if set == name then
+				table.remove(exclude, i)
+				if #exclude < 1 then
+					info.exclude[parent] = nil
 				end
+
+				Combuctor:SendMessage('COMBUCTOR_CONFIG_SUBSET_ADD', key, name, parent)
+				break
 			end
 		end
 	end
 end
 
-local function RemoveSubSet(sets, name, parent)
-	for i,set in pairs(sets) do
-		if set.name == parent then
-			if set.exclude then
-				for j,child in pairs(set.exclude) do
-					if child == name then
-						return
-					end
-				end
-				table.insert(set.exclude, name)
-			else
-				set.exclude = {name}
-			end
-			Combuctor:SendMessage('COMBUCTOR_CONFIG_SUBSET_ADD', key, name, parent)
-		end
-	end
-end
+local function RemoveSubSet(name, parent)
+	local info = Combuctor:GetProfile()[key]
+	local exclude = info.exclude[parent]
 
---list button
-local function ListButton_OnClick(self)
-	if self.parent then
-		if self:GetChecked() then
-			AddSubSet(profile[key].sets, self.name, self.parent)
-		else
-			RemoveSubSet(profile[key].sets, self.name, self.parent)
+	if exclude then
+		for i,set in pairs(exclude) do
+			if set == name then
+				return
+			end
 		end
+		table.insert(exclude, name)
 	else
-		selected[self.name] = not selected[self.name]
-		self:GetParent():UpdateList()
+		info.exclude[parent] = {name}
 	end
+	Combuctor:SendMessage('COMBUCTOR_CONFIG_SUBSET_REMOVE', key, name, parent)
 end
 
-local function HasSet(sets, name, parent)
-	for i,set in pairs(sets) do
-		if parent then
-			if set.name == parent then
-				if set.exclude then
-					for j,child in pairs(set.exclude) do
-						if child == name then
-							return false
-						end
-					end
-				end
-				return true
-			end
-		elseif set.name == name then
+local function HasSet(name)
+	local info = Combuctor:GetProfile()[key]
+
+	for i,setName in pairs(info.sets) do
+		if setName == name then
 			return true
 		end
 	end
 	return false
 end
 
+local function HasSubSet(name, parent)
+	local info = Combuctor:GetProfile()[key]
+	local exclude = info.exclude[setName]
+
+	if exclude then
+		for j,child in pairs(exclude) do
+			if child == name then
+				return false
+			end
+		end
+	end
+	return true
+end
+
+--list button
+local function ListButtonCheck_OnClick(self)
+	local set = self:GetParent().set
+	if set.parent then
+		if self:GetChecked() then
+			AddSubSet(set.name, set.parent)
+		else
+			RemoveSubSet(set.name, set.parent)
+		end
+	else
+		if self:GetChecked() then
+			AddSet(set.name)
+		else
+			RemoveSet(set.name)
+		end
+	end
+end
+
+local function ListButtonToggle_OnClick(self)
+	local set = self:GetParent().set
+
+	selected[set.name] = not selected[set.name]
+	self:GetParent():GetParent():UpdateList()
+end
+
 local function ListButton_Set(self, set)
-	self.name = set.name
-	self.icon = set.icon
-	self.parent = set.parent
+	self.set = set
 
 	if set.icon then
-		getglobal(self:GetName() .. 'Text'):SetFormattedText('|T%s:%d|t %s', set.icon, 32, set.name)
+		getglobal(self.check:GetName() .. 'Text'):SetFormattedText('|T%s:%d|t %s', set.icon, 28, set.name)
 	else
-		getglobal(self:GetName() .. 'Text'):SetText(set.name)
+		getglobal(self.check:GetName() .. 'Text'):SetText(set.name)
 	end
 
-	self:SetChecked(HasSet(profile[key].sets, set.name, set.parent))
+	if set.parent then
+		self.toggle:Hide()
+	else
+		self.toggle:Show()
+
+		if selected[set.name] then
+			self.toggle:SetNormalTexture('Interface\\Buttons\\UI-MinusButton-UP')
+			self.toggle:SetPushedTexture('Interface\\Buttons\\UI-MinusButton-DOWN')
+		else
+			self.toggle:SetNormalTexture('Interface\\Buttons\\UI-PlusButton-UP')
+			self.toggle:SetPushedTexture('Interface\\Buttons\\UI-PlusButton-DOWN')
+		end
+	end
+
+	if set.parent then
+		self.check:SetChecked(HasSubSet(set.name, set.parent))
+	else
+		self.check:SetChecked(HasSet(set.name))
+	end
 end
 
 local function ListButton_Create(id, parent)
-	local b = CreateFrame('CheckButton', parent:GetName() .. 'Button' .. id, parent, 'InterfaceOptionsCheckButtonTemplate')
-	b:SetScript('OnClick', ListButton_OnClick)
+	local name = format('%sButton%d', parent:GetName(), id)
+	local b = CreateFrame('Frame', name, parent)
+	b:SetWidth(200)
+	b:SetHeight(24)
 	b.Set = ListButton_Set
+
+	local toggle = CreateFrame('Button', nil, b)
+	toggle:SetPoint('LEFT', b)
+	toggle:SetWidth(14)
+	toggle:SetHeight(14)
+	toggle:SetNormalTexture('Interface\\Buttons\\UI-PlusButton-UP')
+	toggle:SetPushedTexture('Interface\\Buttons\\UI-PlusButton-DOWN')
+	toggle:SetHighlightTexture('Interface\\Buttons\\UI-PlusButton-Hilight')
+	toggle:SetScript('OnClick', ListButtonToggle_OnClick)
+	b.toggle = toggle
+
+	local check = CreateFrame('CheckButton', name .. 'Check', b, 'InterfaceOptionsCheckButtonTemplate')
+	check:SetScript('OnClick', ListButtonCheck_OnClick)
+	check:SetPoint('LEFT', toggle, 'RIGHT', 4, 0)
+	b.check = check
 
 	return b
 end
@@ -144,8 +202,8 @@ local function Panel_UpdateList(self)
 		local button = self.buttons[i]
 		button:Set(items[i + offset])
 
-		local offLeft = button.parent and 24 or 0
-		button:SetPoint('TOPLEFT', 14 + offLeft, -(64 + button:GetHeight() * i))
+		local offLeft = button.set.parent and 24 or 0
+		button:SetPoint('TOPLEFT', 14 + offLeft, -(86 + button:GetHeight() * i))
 		button:Show()
 
 		i = i + 1
@@ -158,6 +216,39 @@ local function Panel_UpdateList(self)
 	FauxScrollFrame_Update(scrollFrame, #items, MAX_ITEMS, self.buttons[1]:GetHeight())
 end
 
+local info = {}
+local function AddItem(text, value, func, checked, arg1)
+	info.text = text
+	info.func = func
+	info.value = value
+	info.checked = checked
+	info.arg1 = arg1
+	UIDropDownMenu_AddButton(info)
+end
+
+local function AddFrameSelector(self)
+	local dd = self:NewDropdown('Frame')
+
+	dd:SetScript('OnShow', function(self)
+		UIDropDownMenu_SetWidth(self, 110)
+		UIDropDownMenu_Initialize(self, self.Initialize)
+		UIDropDownMenu_SetSelectedValue(self, key)
+	end)
+
+	local function Key_OnClick(self)
+		key = self.value
+		UIDropDownMenu_SetSelectedValue(dd, self.value)
+		dd:GetParent():UpdateList()
+	end
+
+	function dd:Initialize()
+		AddItem(L.Inventory, 'inventory', Key_OnClick, 'inventory' == key)
+		AddItem(L.Bank, 'bank', Key_OnClick, 'bank' == key)
+	end
+	return dd
+end
+
+
 do
 	local panel = Combuctor.Options
 	panel.UpdateList = Panel_UpdateList
@@ -166,13 +257,16 @@ do
 
 	local name = panel:GetName()
 
+	local dropdown = AddFrameSelector(panel)
+	dropdown:SetPoint('TOPLEFT', 6, -72)
+
 	local scroll = CreateFrame('ScrollFrame', name .. 'ScrollFrame', panel, 'FauxScrollFrameTemplate')
 	scroll:SetScript('OnVerticalScroll', function(self, arg1)
 		FauxScrollFrame_OnVerticalScroll(self, arg1, height + offset, function()
 			panel:UpdateList()
 		end)
 	end)
-	scroll:SetPoint('TOPLEFT', 6, -96)
+	scroll:SetPoint('TOPLEFT', 6, -92)
 	scroll:SetPoint('BOTTOMRIGHT', -32, 8)
 	panel.scrollFrame = scroll
 
