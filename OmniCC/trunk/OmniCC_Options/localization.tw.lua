c9831b5d467532e990a9d24868dd2ab10a512bf0
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-10-19 02:20:17 +0000

    added Chinese traditional localization

---
+++ OmniCC/trunk/OmniCC_Options/localization.tw.lua
@@ -0,0 +1,23 @@
+﻿------------------------------------------------------------------------------- 
+-- OmniCC_Options 
+-- Traditional Chinese localization 
+------------------------------------------------------------------------------- 
+
+if GetLocale() == "zhTW" then 
+	local L = OMNICC_LOCALS 
+	L.Thin = '?' 
+	L.Thick = '?' 
+	L.ShowModels = '??????' 
+	L.ShowPulse = '??????' 
+	L.UseMMSS = '?? ?:? ??' 
+	L.MinDuration = '????' 
+	L.MinScale = '????' 
+	L.Display = '??' 
+	L.FontFace = '??' 
+	L.FontOutline = '????' 
+	L.FontSize = '????' 
+	L.Font = '??' 
+	L.ColorsAndScaling = '?????' 
+	L.UnderFiveSeconds = '??5?' 
+	L.OptionsTitle = "??????" 
+end
