2033d56ec5db205684451ea0a4fd750888ff46ad
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-10-26 22:06:45 +0000

    updated french translation

--- Dominos/trunk/Dominos_Config/localization/localization.fr.lua
+++ Dominos/trunk/Dominos_Config/localization/localization.fr.lua
@@ -4,7 +4,7 @@
 
 	English: Default language
 --]]
--- French version (by Kubik) 2008-08-11
+-- French version (by Kubik) 2008-10-19
 -- à = \195\160
 -- â = \195\162
 -- ç = \195\167
@@ -21,7 +21,8 @@ if not L then return end
 
 L.Scale = 'Echelle'
 L.Opacity = 'Opacit\195\169'
-L.FadedOpacity = 'Opacit\195\169 att\195\169nu\195\169e'
+--L.FadedOpacity = 'Opacit\195\169 att\195\169nu\195\169e'
+L.FadedOpacity = 'Opacit\195\169 hors survol souris'
 L.Visibility = 'Visibilit\195\169'
 L.Spacing = 'Espacement'
 L.Padding = 'Remplissage'
@@ -46,16 +47,20 @@ L.RCUToT = 'Cible de la cible'
 L.EnterName = 'Entrer Nom'
 L.PossessBar = 'Barre des possessions'
 L.Profiles = 'Profils'
-L.ProfilesPanelDesc = 'Permet de g\195\169rer les affichages Dominos enregistr\195\169s'
-L.SelfcastKey = 'Touche Selfcast'
-L.QuickMoveKey = 'Touche Quick Move'
+L.ProfilesPanelDesc = 'Gestion des affichages Dominos enregistr\195\169s'
+L.SelfcastKey = 'Touche auto-ciblage'
+L.QuickMoveKey = 'Touche dplct boutons'
 L.ShowMacroText = 'Montrer le nom de la macro'
 L.ShowBindingText = 'Montrer les raccourcis'
 L.ShowEmptyButtons = 'Montrer les boutons vides'
 L.LockActionButtons = 'Verrouiller boutons d\'action'
-L.EnterBindingMode = 'Lier touches...'
-L.EnterConfigMode = 'Configuration'
+L.EnterBindingMode = 'Raccourcis...'
+L.EnterConfigMode = 'Configuration...'
 L.ExitConfigMode = 'Arr\195\170t configuration'
 L.ActionBarSettings = 'R\195\169glages barre d\'action %d'
 L.BarSettings = 'R\195\169glages barre %s'
 L.ShowTooltips = 'Montrer infobulles des boutons'
+L.OneBag = 'Un seul sac'
+L.ShowKeyring = 'Montrer trousseau de clefs'
+L.StickyBars = 'Barres autocollantes'
+L.ShowMinimapButton = 'Montrer le bouton Minimap'
