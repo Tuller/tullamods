24eff5481c5b1926117bd4401edc70ed281d7ad2
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-10-27 23:50:42 +0000

    updated chinese localization

--- OmniCC/trunk/OmniCC_Options/localization.tw.lua
+++ OmniCC/trunk/OmniCC_Options/localization.tw.lua
@@ -1,23 +1,23 @@
-﻿------------------------------------------------------------------------------- 
--- OmniCC_Options 
--- Traditional Chinese localization 
-------------------------------------------------------------------------------- 
+﻿--[[
+	OmniCC Options Localizion
+		T-Chinese by NightOwl
+--]]
 
-if GetLocale() == "zhTW" then 
-	local L = OMNICC_LOCALS 
-	L.Thin = '?' 
-	L.Thick = '?' 
-	L.ShowModels = '??????' 
-	L.ShowPulse = '??????' 
-	L.UseMMSS = '?? ?:? ??' 
-	L.MinDuration = '????' 
-	L.MinScale = '????' 
-	L.Display = '??' 
-	L.FontFace = '??' 
-	L.FontOutline = '????' 
-	L.FontSize = '????' 
-	L.Font = '??' 
-	L.ColorsAndScaling = '?????' 
-	L.UnderFiveSeconds = '??5?' 
-	L.OptionsTitle = "??????" 
+if GetLocale() == 'zhTW' then
+	local L = OMNICC_LOCALS
+	L.Thin = '幼線'
+	L.Thick = '粗線'
+	L.ShowModels = '顯示冷卻模組'
+	L.ShowPulse = '使用閃爍模式'
+	L.UseMMSS = '使用 MM:SS 格式'
+	L.MinDuration = '最少時間'
+	L.MinScale = '最少刻度'
+	L.Display = '顯示'
+	L.FontFace = '字形'
+	L.FontOutline = '字形外觀'
+	L.FontSize = '字形大小'
+	L.Font = '字形'
+	L.ColorsAndScaling = '顏色與刻度'
+	L.UnderFiveSeconds = '少於5秒'
+	L.OptionsTitle = 'OmniCC 設定'
 end
