6e86f504ef631c5efb84fa2a1d2d56156e61f6dc
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-11-18 22:18:42 +0000

    chinese

--- Bagnon/trunk/Bagnon_Options/localization/localization.cn.lua
+++ Bagnon/trunk/Bagnon_Options/localization/localization.cn.lua
@@ -1,39 +1,39 @@
-﻿--[[
+--[[
 	Bagnon Main Options Localization file
 --]]
 
 --[[
 	THIS FILE IS ENCODED IN UTF-8
 
-	Bagnon Localization file: Chinese 
-		Credit goes to Diablohu
+	Bagnon Localization file: Chinese Simplified
+		Credit: Diablohu, yleaf
 	
-	Last Update: 4.11.2007
+	Last Update: Nov 16, 2008
 --]]
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-Options', 'zhCN')
 if not L then return end
 	
-L.Title = "Bagnon 设置";
+L.Title = 'Bagnon 设置'
+L.Display = '显示设置'
+L.Events = '事件设置'
 
-L.ShowBorders = "显示物品质量框"
+L.FrameSettings = '框体设置'
+L.Lock = '锁定位置'
+L.Toplevel = '总在最前'
+L.BackgroundColor = '背景颜色'
+L.FrameLevel = '层'
+L.Opacity = '透明度'
+L.Scale = '缩放'
+L.Spacing = '间距'
+L.Cols = '列数'
+L.ReverseSort = '反向排列'
+L.ShowBorders = '物品品质着色'
 
-L.ReplaceBags = "替换默认背包界面"
-
-L.AtBank = "在银行时";
-L.AtVendor = "与商贩对话时";
-L.AtAH = "在拍卖行时";
-L.AtMail = "打开邮箱时";
-L.AtTrade = "交易时";
-L.AtCraft = "制作物品时";
-
---menu text
-L.Lock = "锁定位置"
-L.Toplevel = "总在最前"
-L.BackgroundColor = "背景颜色"
-L.FrameLevel = "层"
-L.Opacity = "透明度"
-L.Scale = "缩放"
-L.Spacing = "间距"
-L.Cols = "列数"
-L.ReverseSort = "反向排列"
+L.ReplaceBags = '替换背包'
+L.AtBank = '打开银行时'
+L.AtVendor = '与商贩对话时'
+L.AtAH = '打开拍卖行时'
+L.AtMail = '打开邮箱时'
+L.AtTrade = '交易时'
+L.AtCraft = '制作物品时'
