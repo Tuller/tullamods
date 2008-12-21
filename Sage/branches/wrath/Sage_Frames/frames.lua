d367b96e60de43eaee9ea58ccb4f167667c59dfb
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2008-12-21 02:05:12 +0000

    unnecessary OO

--- Sage/branches/wrath/Sage_Frames/frames.lua
+++ Sage/branches/wrath/Sage_Frames/frames.lua
@@ -4,12 +4,11 @@ local POWERBAR_HEIGHT = 12
 local INFOBAR_HEIGHT = 18
 local BORDER_SIZE = 2
 
-local function IsFriendlyUnit(unit)
-	return unit == 'player' or unit == 'pet' or unit:match('party%d')
-end
+
+--[[ Generic Major Frame ]]--
 
 function MajorFrame:New(unit)
-	local f = self.super.New(self, unit)
+	local f = MajorFrame.super.New(self, unit)
 	f:SetWidth(140)
 	f:SetHeight(INFOBAR_HEIGHT + HEALTHBAR_HEIGHT + POWERBAR_HEIGHT + BORDER_SIZE*2)
 
@@ -23,7 +22,7 @@ function MajorFrame:New(unit)
 	health:SetPoint('BOTTOMRIGHT', info, 'BOTTOMRIGHT', 0, -HEALTHBAR_HEIGHT)
 	f.health = health
 
-	local power = Sage.PowerBar:NewFrequent(f, 'GameFontHighlightSmall')
+	local power = Sage.PowerBar:New(f, 'GameFontHighlightSmall')
 	power:SetPoint('TOPLEFT', health, 'BOTTOMLEFT', 0, -BORDER_SIZE)
 	power:SetPoint('BOTTOMRIGHT', health, 'BOTTOMRIGHT', 0, -POWERBAR_HEIGHT)
 	f.power = power
@@ -44,70 +43,99 @@ function MajorFrame:New(unit)
 	click:SetPoint('BOTTOMRIGHT', health, 'BOTTOMRIGHT', 0, -POWERBAR_HEIGHT)
 	f.click = click
 
-	if IsFriendlyUnit(unit) then
-		if unit ~= 'player' then
-			local buff = Sage.AuraContainer:New('Buffs', f, 'HELPFUL')
-			buff:SetPoint('TOPLEFT', power, 'BOTTOMLEFT', 0, -2)
-			buff:SetPoint('BOTTOMRIGHT', power, 'BOTTOMRIGHT', 0, -(17*2 + 2))
-			f.buff = buff
-
-			local debuff = Sage.AuraContainer:New('Debuffs', f, 'HARMFUL')
-			debuff:SetPoint('TOPLEFT', health, 'TOPRIGHT', 1, 0)
-			debuff:SetPoint('BOTTOMLEFT', power, 'BOTTOMRIGHT', 1, 0)
-			debuff:SetWidth(30)
-			f.debuff = debuff
-
-			f:SetHeight(f:GetHeight() + 17*2 + 1)
-		end
-	else
-		if unit == 'target' then
-			local combo = Sage.ComboFrame:New(f, 'GameFontHighlightSmall')
-			combo:SetPoint('TOPRIGHT', f, 'TOPLEFT')
-			f.combo = combo
-		end
-
-		local npc = Sage.NPCInfoBar:New(f, 'GameFontHighlightSmall')
-		npc:SetPoint('TOPLEFT', power, 'BOTTOMLEFT', 0, -2)
-		npc:SetPoint('TOPRIGHT', power, 'BOTTOMRIGHT', 0, -2)
-		npc:SetHeight(12)
-		f.npc = npc
-
-		power:HookScript('OnShow', function()
-			npc:SetPoint('TOPLEFT', power, 'BOTTOMLEFT', 0, -2)
-			npc:SetPoint('TOPRIGHT', power, 'BOTTOMRIGHT', 0, -2)
-			threat:SetPoint('BOTTOMRIGHT', power, BORDER_SIZE, -BORDER_SIZE)
-		end)
-
-		power:SetScript('OnHide', function()
-			npc:SetPoint('TOPLEFT', health, 'BOTTOMLEFT', 0, -2)
-			npc:SetPoint('TOPRIGHT', health, 'BOTTOMRIGHT', 0, -2)
-			threat:SetPoint('BOTTOMRIGHT', health, BORDER_SIZE, -BORDER_SIZE)
-		end)
-
-		local debuff = Sage.AuraContainer:New('Debuffs', f, 'HARMFUL|PLAYER', 'HELPFUL|PLAYER')
-		debuff:SetPoint('TOPLEFT', npc, 'BOTTOMLEFT', 0, -2)
-		debuff:SetPoint('BOTTOMRIGHT', npc, 'BOTTOMRIGHT', 0, -(17*2 + 2))
-		f.debuff = debuff
-
-		local buff = Sage.AuraContainer:New('Buffs', f, 'HELPFUL', 'HARMFUL')
-		buff:SetPoint('TOPLEFT', health, 'TOPRIGHT', 1, 0)
-		buff:SetPoint('BOTTOMLEFT', power, 'BOTTOMRIGHT', 1, 0)
-		buff:SetWidth(18 * 4)
-		f.buff = buff
-
-		f:SetHeight(f:GetHeight() + 14 + 17*2 + 2)
-	end
-
 	return f
 end
 
 function MajorFrame:UpdateAll()
 	self.info:UpdateAll()
 	self.health:UpdateAll()
-	self.power:Update()
+	self.power:UpdateAll()
+
 	self.debuff:Update()
 	self.buff:Update()
-	self.cast:Update()
+
+	if self.cast then
+		self.cast:Update()
+	end
+end
+
+
+--[[ Friendly Unit Frame ]]--
+
+local FriendFrame = Sage:CreateClass('Frame', MajorFrame)
+
+function FriendFrame:New(unit)
+	local f = FriendFrame.super.New(self, unit)
+
+	local buff = Sage.AuraContainer:New('Buffs', f, 'HELPFUL')
+	buff:SetPoint('TOPLEFT', power, 'BOTTOMLEFT', 0, -2)
+	buff:SetPoint('BOTTOMRIGHT', power, 'BOTTOMRIGHT', 0, -(17*2 + 2))
+	f.buff = buff
+
+	local debuff = Sage.AuraContainer:New('Debuffs', f, 'HARMFUL')
+	debuff:SetPoint('TOPLEFT', health, 'TOPRIGHT', 1, 0)
+	debuff:SetPoint('BOTTOMLEFT', power, 'BOTTOMRIGHT', 1, 0)
+	debuff:SetWidth(30)
+	f.debuff = debuff
+
+	f:SetHeight(f:GetHeight() + 17*2 + 1)
+	return f
+end
+
+
+--[[ Targeting Frame ]]--
+
+local TargetFrame = Sage:CreateClass('Frame', MajorFrame)
+
+function TargetFrame:New(unit)
+	local f = TargetFrame.super.New(self, unit)
+
+	if unit == 'target' then
+		local combo = Sage.ComboFrame:New(f, 'GameFontHighlightSmall')
+		combo:SetPoint('TOPRIGHT', f, 'TOPLEFT')
+		f.combo = combo
+	end
+
+	local npc = Sage.NPCInfoBar:New(f, 'GameFontHighlightSmall')
+	npc:SetPoint('TOPLEFT', power, 'BOTTOMLEFT', 0, -2)
+	npc:SetPoint('TOPRIGHT', power, 'BOTTOMRIGHT', 0, -2)
+	npc:SetHeight(12)
+	f.npc = npc
+
+	--shift the NPC frame up when the power frame is hidden
+	local power = f.power
+	local threat = f.threat
+	local health = f.health
+
+	power:HookScript('OnShow', function()
+		npc:SetPoint('TOPLEFT', power, 'BOTTOMLEFT', 0, -2)
+		npc:SetPoint('TOPRIGHT', power, 'BOTTOMRIGHT', 0, -2)
+		threat:SetPoint('BOTTOMRIGHT', power, BORDER_SIZE, -BORDER_SIZE)
+	end)
+
+	power:SetScript('OnHide', function()
+		npc:SetPoint('TOPLEFT', health, 'BOTTOMLEFT', 0, -2)
+		npc:SetPoint('TOPRIGHT', health, 'BOTTOMRIGHT', 0, -2)
+		threat:SetPoint('BOTTOMRIGHT', health, BORDER_SIZE, -BORDER_SIZE)
+	end)
+
+	local debuff = Sage.AuraContainer:New('Debuffs', f, 'HARMFUL|PLAYER', 'HELPFUL|PLAYER')
+	debuff:SetPoint('TOPLEFT', npc, 'BOTTOMLEFT', 0, -2)
+	debuff:SetPoint('BOTTOMRIGHT', npc, 'BOTTOMRIGHT', 0, -(17*2 + 2))
+	f.debuff = debuff
+
+	local buff = Sage.AuraContainer:New('Buffs', f, 'HELPFUL', 'HARMFUL')
+	buff:SetPoint('TOPLEFT', health, 'TOPRIGHT', 1, 0)
+	buff:SetPoint('BOTTOMLEFT', power, 'BOTTOMRIGHT', 1, 0)
+	buff:SetWidth(18 * 4)
+	f.buff = buff
+
+	f:SetHeight(f:GetHeight() + 14 + 17*2 + 2)
+	return f
+end
+
+function TargetFrame:UpdateAll()
+	TargetFrame.super.UpdateAll(self)
 
 	if self.npc then
 		self.npc:Update()
@@ -153,21 +181,21 @@ function module:OnLoad()
 	local player = MajorFrame:New('player')
 	RegisterStateDriver(player, 'unit', '[target=vehicle,exists]vehicle;player')
 
-	local target = MajorFrame:New('target')
+	local target = TargetFrame:New('target')
 	target:SetScript('OnEvent', target.UpdateAll)
 	target:RegisterEvent('PLAYER_TARGET_CHANGED')
 
-	local focus = MajorFrame:New('focus')
+	local focus = TargetFrame:New('focus')
 	focus:SetScript('OnEvent', focus.UpdateAll)
 	focus:RegisterEvent('PLAYER_FOCUS_CHANGED')
 
-	local pet = MajorFrame:New('pet')
+	local pet = FriendFrame:New('pet')
 	RegisterStateDriver(pet, 'unit', '[target=vehicle,exists]player;pet')
 	pet:SetScript('OnEvent', pet.UpdateAll)
 	pet:RegisterEvent('UNIT_PET')
 
 	for i = 1, MAX_PARTY_MEMBERS do
-		local f = MajorFrame:New('party' .. i)
+		local f = FriendFrame:New('party' .. i)
 		f:RegisterEvent('PARTY_MEMBERS_CHANGED')
 		f:RegisterEvent('RAID_ROSTER_UPDATE')
 		f:SetScript('OnEvent', f.UpdateAll)
