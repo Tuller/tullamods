62fbcfc825b4ddfb0aee28a108c910e8ecd75b13
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-03-02 18:27:56 +0000

    Bye, Sage 2

---
+++ Sage/tags/trunk/Sage/localization.lua
@@ -0,0 +1,60 @@
+--[[
+	Bongos' Localization File
+		English Language (default)
+--]]
+
+SAGE_LOCALS = {}
+local L = SAGE_LOCALS
+
+--system messages
+L.NewPlayer = "Created new profile for %s"
+L.Updated = "Updated to v%s"
+L.UpdatedIncompatible = "Updating from an incompatible version. Default settings loaded"
+
+--profiles
+L.ProfileCreated = "Created new profile \"%s\""
+L.ProfileLoaded = "Set profile to \"%s\""
+L.ProfileDeleted = "Deleted profile \"%s\""
+L.ProfileCopied = "Copied settings from \"%s\" to \"%s\""
+L.ProfileReset = "Reset profile \"%s\""
+L.CantDeleteCurrentProfile = "Cannot delete the current profile"
+
+--slash command help
+L.LockFramesDesc = "Toggles locking frame positions"
+L.StickyFramesDesc = "Toggles auto anchoring frames"
+
+L.SetScaleDesc = "Sets the scale of <frameList>"
+L.SetAlphaDesc = "Sets the opacity of <frameList>"
+
+--slash commands for profiles
+L.ResetDesc = "Loads default settings"
+L.SaveDesc = "Saves current settings as, and switches to <profile>"
+L.SetDesc = "Switches settings to <profile>"
+L.CopyDesc = "Copies settings from <profile>"
+L.DeleteDesc = "Deletes <profile>"
+L.ResetDesc = "Returns to default settings"
+L.ListDesc = "Lists all profiles"
+L.AvailableProfiles = "Available Profiles"
+L.PrintVersionDesc = "Prints the current Bongos version"
+
+--dragFrame tooltips
+L.SetAlpha = "<Mousewheel> to set opacity (%.2f)"
+
+--Menu Stuff
+L.Scale = "Scale"
+L.Opacity = "Opacity"
+L.Visibility = "Visibility"
+L.Width = "Width"
+L.Layout = "Layout"
+
+--statusbar text
+L.Dead = "Dead"
+L.Offline = "Offline"
+L.Ghost = "Ghost"
+L.FeignDeath = "Feign"
+
+L.Boss = "Boss"
+L.Rare = "Rare"
+L.Elite = "Elite"
+L.RareElite = "Rare Elite"
+L.UnknownCreature = "Not specified"
