afd6a37bcab4723141c0de0f3195bdc9cb756ee4
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-04-11 17:50:35 +0000

    ghetto merge

--- OmniCC_Shine/trunk/OmniCC_Shine/shine.lua
+++ OmniCC_Shine/trunk/OmniCC_Shine/shine.lua
@@ -6,62 +6,42 @@ local OmniCC = _G['OmniCC']
 assert(OmniCC, 'Could not find an instance of OmniCC')
 assert(not OmniCC.OnFinishCooldown, 'Another finish effect is already loaded')
 
---[[ the Shine object ]]--
+
+--[[ the shine object ]]--
 
 local Shine = OmniCC:CreateClass('Frame')
-Shine.SCALE = 5 --how big the thing should get
-Shine.DURATION = 1 --how long the effect should last (in seconds)
-Shine.TEXTURE = [[Interface\Cooldown\star4]] --the graphic of the effect
+local ANIMATION_TEMPLATE = 'OmniCCAnimationTemplate_Shine'
+local TEXTURE = [[Interface\Cooldown\star4]]
 
 function Shine:New(parent)
-	local f = self:Bind(CreateFrame('Frame', nil, parent))
+	local f = self:Bind(CreateFrame('Frame', nil, parent)) 
 	f:SetAllPoints(parent)
 	f:SetToplevel(true)
 	f:Hide()
-
-	f:SetScript('OnUpdate', f.OnUpdate)
+	
+	f.ani = f:CreateAnimationGroup(nil, ANIMATION_TEMPLATE)
 	f:SetScript('OnHide', f.OnHide)
 
 	local icon = f:CreateTexture(nil, 'OVERLAY')
 	icon:SetPoint('CENTER')
 	icon:SetBlendMode('ADD')
-	icon:SetTexture(f.TEXTURE)
-	f.icon = icon
+	icon:SetAllPoints(f)
+	icon:SetTexture(TEXTURE)
 
 	return f
 end
 
-function Shine:Start(texture)
-	if not self:IsShown() then
-		--reset everything to defaults
-		self.icon:SetAlpha(1)
-		self.icon:SetHeight(self:GetHeight())
-		self.icon:SetWidth(self:GetWidth())
-		
-		--show the effect, start the cycle
+function Shine:Start()
+	if not self.ani:IsPlaying() then
 		self:Show()
+		self.ani:Play()
 	end
 end
 
---omg speed
-function Shine:OnUpdate(elapsed)
-	local icon = self.icon
-	local newAlpha = icon:GetAlpha() - (elapsed/self.DURATION)
-
-	--the effect is will still be visible, adjust size, etc
-	if newAlpha > 0 then
-		icon:SetAlpha(newAlpha)
-
-		local multiplier = newAlpha * self.SCALE
-		icon:SetHeight(self:GetHeight() * multiplier)
-		icon:SetWidth(self:GetWidth() * multiplier)
-	else
-		self:Hide()
-	end
-end
-
---this may look stupid, but it handles the case of the Shine no longer being visible due to its parent hiding
 function Shine:OnHide()
+	if self.ani:IsPlaying() then
+		self.ani:Finish()
+	end
 	self:Hide()
 end
 
@@ -69,8 +49,7 @@ end
 --[[ omnicc hooks ]]--
 
 do
-	--laziness thing to prevent creation of multiple effects for the same parent object
-	local effects = setmetatable({}, {__index = function(t, k)
+	local shines = setmetatable({}, {__index = function(t, k)
 		local f = Shine:New(k)
 		t[k] = f
 		return f
@@ -79,7 +58,7 @@ do
 	OmniCC.OnFinishCooldown = function(self, timer)
 		local parent = timer:GetParent()
 		if parent:IsVisible() then
-			effects[parent]:Start()
+			shines[parent]:Start()
 		end
 	end
 end
