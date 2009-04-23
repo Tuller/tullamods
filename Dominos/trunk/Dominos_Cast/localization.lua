a49e80a8eb6fcc125012dbb87c1e925e1bb86578
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-04-23 23:12:01 +0000

    Added Radded Russian localization

--- Dominos/trunk/Dominos_Cast/localization.lua
+++ Dominos/trunk/Dominos_Cast/localization.lua
@@ -10,6 +10,8 @@ elseif locale == 'zhCN' then
 	Dominos_SHOW_TIME = '显示时间'
 elseif locale == 'esES' then
 	Dominos_SHOW_TIME = 'Mostrar tiempo'
+elseif locale == 'ruRU' then
+	Dominos_SHOW_TIME = 'Показать время'
 else
 	Dominos_SHOW_TIME = 'Show Time'
 end
