0ae28e00be657b3082125d71a587e16126964b93
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-05-10 16:12:00 +0000

    updated tw localization

--- OmniCC/trunk/OmniCC_Options/localization/localization.tw.lua
+++ OmniCC/trunk/OmniCC_Options/localization/localization.tw.lua
@@ -8,7 +8,7 @@ if GetLocale() == 'zhTW' then
 	L.Thin = '幼線'
 	L.Thick = '粗線'
 	L.ShowModels = '顯示冷卻模組'
-	L.ShowPulse = '使用閃爍模式'
+	L.ShowPulse = '使用脈衝模式'
 	L.UseMMSS = '使用 MM:SS 格式'
 	L.MinDuration = '最少時間的時值'
 	L.MinScale = '最少刻度'
@@ -21,4 +21,5 @@ if GetLocale() == 'zhTW' then
 	L.UnderFiveSeconds = '少於5秒'
 	L.OptionsTitle = 'OmniCC 設定'
 	L.MinEffectDuration = '效果完成的時值'
+	L.ShowTenthsOfSeconds = '以十分之一秒為單位顯示'
 end
