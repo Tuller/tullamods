a1d591ae4f93bb54e7688d41bcd8c1a163a79823
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-06-15 22:51:44 +0000

    updated localization

---
+++ Bagnon2/trunk/Bagnon_Config/localization/localization.cn.lua
@@ -0,0 +1,28 @@
+﻿--[[
+	THIS FILE IS ENCODED IN UTF-8
+
+	Bagnon Config Localization Information: Chinese Simplified
+		Credits: Diablohu, yleaf@cwdg(yaroot@gmail.com), 狂飙@cwdg(networm@qq.com)
+
+	Last Update: 2009/06/15 by 狂飙@cwdg(networm@qq.com)
+
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-Config', 'zhCN')
+if not L then return end
+
+L.Columns = '列数'
+L.Scale = '缩放'
+L.Spacing = '间距'
+L.Opacity = '透明度'
+L.FrameColor = '窗口颜色'
+L.FrameBorderColor = '窗口边框颜色'
+L.Frame = '窗口'
+L.Inventory = '背包'
+L.Bank = '银行'
+L.KeyRing = '钥匙链'
+L.LockFramePositions = '锁定位置'
+L.ShowEmptyItemSlotBackground = '显示空格背景材质'
+L.HighlightItemsByQuality = '按物品品质高亮物品'
+L.HighlightQuestItems = '高亮任务物品'
+L.ReverseSlotOrdering = '反向排列'
---
+++ Bagnon2/trunk/Bagnon_Config/localization/localization.tw.lua
@@ -0,0 +1,28 @@
+﻿--[[
+	THIS FILE IS ENCODED IN UTF-8
+
+	Bagnon Config Localization Information: Chinese Traditional
+		Credits: yleaf@cwdg(yaroot@gmail.com), 狂飆@cwdg(networm@qq.com)
+
+	Last Update: 2009/06/15 by 狂飆@cwdg(networm@qq.com)
+
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-Config', 'zhTW')
+if not L then return end
+
+L.Columns = '列數'
+L.Scale = '縮放'
+L.Spacing = '間距'
+L.Opacity = '透明度'
+L.FrameColor = '窗口顏色'
+L.FrameBorderColor = '窗口邊框顏色'
+L.Frame = '窗口'
+L.Inventory = '背包'
+L.Bank = '銀行'
+L.KeyRing = '鑰匙鏈'
+L.LockFramePositions = '鎖定位置'
+L.ShowEmptyItemSlotBackground = '顯示空格背景材質'
+L.HighlightItemsByQuality = '按物品品質高亮物品'
+L.HighlightQuestItems = '高亮任務物品'
+L.ReverseSlotOrdering = '反向排列'
