6e0e624a5f0824e128ca5d15fd0be3ac637e144c
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-06-15 22:51:37 +0000

    updated localization

--- Bagnon2/trunk/Bagnon/localization/localization.cn.lua
+++ Bagnon2/trunk/Bagnon/localization/localization.cn.lua
@@ -1,45 +1,60 @@
---[[
+﻿--[[
 	THIS FILE IS ENCODED IN UTF-8
 
-	Bagnon Localization file: Chinese Simplified
-		Credit: Diablohu, yleaf, 狂飙
-	
-	Last Update: Apr 25, 2009
+	Bagnon Localization Information: Chinese Simplified
+		Credits: Diablohu, yleaf@cwdg(yaroot@gmail.com), 狂飙@cwdg(networm@qq.com)
+
+	Last Update: 2009/06/15 by 狂飙@cwdg(networm@qq.com)
+
 --]]
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon', 'zhCN')
 if not L then return end
 
---bindings
-L.BagnonToggle = "开关 背包"
-L.BanknonToggle = "开关 银行"
+--keybinding text
+L.ToggleBags = '开关 背包'
+L.ToggleBank = '开关 银行'
+L.ToggleKeys = '开关 钥匙链'
+
 
 --system messages
-L.NewUser = "这是该角色第一次使用 Bagnon，已载入默认设置。"
-L.Updated = "已更新到 v%s"
-L.UpdatedIncompatible = "从一个错误的版本更新，已载入默认设置。"
+L.NewUser = '这是该角色第一次使用 Bagnon，已载入默认设置。'
+L.Updated = '已更新到 Bagnon v%s'
+L.UpdatedIncompatible = '由一个不相容版本升级，已载入默认设置。'
 
---errors
-L.ErrorNoSavedBank = "无法打开银行：无可用的存储信息。"
-L.vBagnonLoaded = format("vBagnon 和 Bagnon 不能同时开启。点击 %s 禁用 vBagnon 并重载界面", TEXT(ACCEPT))
 
 --slash commands
-L.Commands = "命令:"
-L.ShowBagsDesc = "开关背包界面"
-L.ShowBankDesc = "开关银行界面"
-L.ShowVersionDesc = '显示当前版本'
+L.Commands = '命令：'
+L.CmdShowInventory = '开关背包界面'
+L.CmdShowBank = '开关银行界面'
+L.CmdShowKeyring = '开关钥匙链界面'
+L.CmdShowVersion = '显示当前版本'
+
 
 --frame text
-L.TitleBank = "%s的银行"
-L.TitleBags = "%s的背包"
-L.ShowBags = "显示包裹"
-L.HideBags = "隐藏包裹"
+L.TitleBags = '%s的背包'
+L.TitleBank = '%s的银行'
+L.TitleKeys = '%s的钥匙链'
+
 
 --tooltips
-L.TipShowMenu = "<右键点击>打开设置菜单"
-L.TipShowSearch = "<双击>进行搜索"
-L.TipShowBag = "<单击>显示"
-L.TipHideBag = "<单击>隐藏"
-L.TipGoldOnRealm = "%s上的总资产"
+L.TipBank = '银行'
+L.TipChangePlayer = '查看其他角色的物品'
+L.TipGoldOnRealm = '%s上的总资产'
+L.TipHideBag = '隐藏包裹'
+L.TipHideBags = '隐藏背包'
+L.TipHideSearch = '隐藏搜索界面'
+L.TipPurchaseBag = '购买银行空位'
+L.TipShowBag = '显示包裹'
+L.TipShowBags = '显示背包'
+L.TipShowMenu = '右击打开设置菜单'
+L.TipShowSearch = '显示搜索界面'
+L.TipShowSearch = '搜索'
+L.TipShowFrameConfig = '打开设置菜单'
+L.Total = '总共'
 
-L.ConfirmReloadUI = '这个设置将在你下次登录时生效'
+--databroker tooltips
+L.TipShowBank = 'Shift-点击 开关银行'
+L.TipShowInventory = '点击 开关背包'
+L.TipShowKeyring = 'Alt-点击 开关钥匙链'
+L.TipShowOptions = '右击 打开设置菜单'
--- Bagnon2/trunk/Bagnon/localization/localization.tw.lua
+++ Bagnon2/trunk/Bagnon/localization/localization.tw.lua
@@ -1,42 +1,63 @@
---[[
-    Bagnon Localization Information: Traditional Chinese Language
-        20071117 by matini< yiting.jheng <at> gmail <dot> com
-        20081201 by yleaf@cwdg(yaroot@gmail.com)
-        20090423 by youngway@水晶之刺
+﻿--[[
+	THIS FILE IS ENCODED IN UTF-8
+
+	Bagnon Localization Information: Chinese Traditional
+	        2007/11/17 by matini< yiting.jheng <at> gmail <dot> com
+	        2008/12/01 by yleaf@cwdg(yaroot@gmail.com)
+	        2009/04/23 by youngway@水晶之刺
+		2009/06/15 by 狂飆@cwdg(networm@qq.com)
+
+	Last Update: 2009/06/15 by 狂飆@cwdg(networm@qq.com)
+
 --]]
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon', 'zhTW')
 if not L then return end
 
-L.BagnonToggle  = "切換背包整合開關"
-L.BanknonToggle  = "切換銀行整合開關"
+--keybinding text
+L.ToggleBags = '開關 背包'
+L.ToggleBank = '開關 銀行'
+L.ToggleKeys = '開關 鑰匙鏈'
+
 
 --system messages
-L.NewUser = "偵測到新的使用者，預設值已載入"
-L.Updated = "已更新到v%s"
-L.UpdatedIncompatible = "由不相容版本升級，預設值已載入"
+L.NewUser = '這是該角色第一次使用 Bagnon，已載入默認設置。'
+L.Updated = '已更新到 Bagnon v%s'
+L.UpdatedIncompatible = '由一個不相容版本升級，已載入默認設置。'
 
---errors
-L.ErrorNoSavedBank = "無法開啟銀行，沒有儲存資訊"
-L.vBagnonLoaded = format("vBagnon 和 Bagnon 不相容。點擊 %s 將 vBagnon 停用並重載你的 UI", TEXT(ACCEPT))
 
 --slash commands
-L.Commands = "指令:"
-L.ShowBagsDesc = "顯示背包"
-L.ShowBankDesc = "顯示銀行"
-L.ShowVersionDesc = '顯示目前版本'
+L.Commands = '命令：'
+L.CmdShowInventory = '開關背包介面'
+L.CmdShowBank = '開關銀行介面'
+L.CmdShowKeyring = '開關鑰匙鏈介面'
+L.CmdShowVersion = '顯示當前版本'
+
 
 --frame text
-L.TitleBank = "%s的銀行"
-L.TitleBags = "%s的背包"
-L.ShowBags = "顯示背包"
-L.HideBags = "隱藏背包"
+L.TitleBags = '%s的背包'
+L.TitleBank = '%s的銀行'
+L.TitleKeys = '%s的鑰匙鏈'
+
 
 --tooltips
-L.TipShowMenu = "<右鍵>設定"
-L.TipShowSearch = "<雙擊>搜尋"
-L.TipShowBag = "<點擊>顯示"
-L.TipHideBag = "<點擊>隱藏"
-L.TipGoldOnRealm = "總計%s"
+L.TipBank = '銀行'
+L.TipChangePlayer = '查看其他角色的物品'
+L.TipGoldOnRealm = '%s上的總資產'
+L.TipHideBag = '隱藏包裹'
+L.TipHideBags = '隱藏背包'
+L.TipHideSearch = '隱藏搜索介面'
+L.TipPurchaseBag = '購買銀行空位'
+L.TipShowBag = '顯示包裹'
+L.TipShowBags = '顯示背包'
+L.TipShowMenu = '右擊打開設置菜單'
+L.TipShowSearch = '顯示搜索介面'
+L.TipShowSearch = '搜索'
+L.TipShowFrameConfig = '打開設置菜單'
+L.Total = '總共'
 
-L.ConfirmReloadUI = '這個設置將在下次登陸時生效'
+--databroker tooltips
+L.TipShowBank = 'Shift-點擊 開關銀行'
+L.TipShowInventory = '點擊 開關背包'
+L.TipShowKeyring = 'Alt-點擊 開關鑰匙鏈'
+L.TipShowOptions = '右擊 打開設置菜單'
