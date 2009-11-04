a37307777f7f48b39bdd9d9b2bb5e73878eab6f3
StingerSoft@gmail.com <StingerSoft@gmail.com@85caf629-fd28-0410-b983-630554dc95c4>
2009-11-04 07:22:11 +0000

    ruRU added

---
+++ Bongos_Minimap/trunk/Bongos_Minimap/localization/localization.ru.lua
@@ -0,0 +1,13 @@
+--[[
+	Bongos Stats Localization file
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('Bongos3-Minimap', 'ruRU')
+if not L then return end
+
+L.ShowMapTitle = 'Показать заглавия'
+L.ShowMapZoom = 'Показать кнопки масштаба'
+L.ShowMapTime = 'Показать индикатор суток'
+L.ShowMapButton = 'Показать кнопку карты'
+L.ShowClock = 'Показать время'
+L.Offset = 'Сдвиг'
