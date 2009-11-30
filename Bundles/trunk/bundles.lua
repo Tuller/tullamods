acf6a432883b623cd098278835908aaf772f2d13
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-11-30 04:31:58 +0000

    commenting and other things (drycode)

--- Bundles/trunk/bundles.lua
+++ Bundles/trunk/bundles.lua
@@ -1,3 +1,10 @@
+--[[
+	bundles.lua
+		A thingy to mass move items from one inventory type to another
+		
+	I'm sure I've looked at both quicksort and bankstack for this
+--]]
+
 local LIS = LibStub('LibItemSearch-1.0')
 local abs = math.abs
 local modf = math.modf
@@ -5,24 +12,23 @@ local tremove = table.remove
 local tinsert = table.insert
 local format = string.format
 
---[[ Utility Methods ]]
+--[[
+	utility stuff
+--]]
 
 local function slotToIndex(bag, slot, link)
-	local slotIndex
-	if bag < 0 then
-		slotIndex = (100 * bag) - slot
-	else
-		slotIndex = (100 * bag) + slot
-	end
-	return slotIndex .. '|' .. link
+	return format('%s,%s|%d', bag, slot, link)
 end
 
 local function indexToSlot(index)
-	local slotIndex, link = index:match('^(%-?%d+)%|(.+)')
-	local bag, slot = modf(slotIndex / 100)
-	slot = abs(slot * 100)
+	local bag, slot, link = index:match('^(%-?%d+)%,(%d+)%|(.+)')
 	return bag, slot, link
 end
+
+
+--[[
+	item searching
+--]]
 
 local function getItemsInSearch(search, bags)
 	local items = {}
@@ -30,6 +36,7 @@ local function getItemsInSearch(search, bags)
 	for _, bag in ipairs(bags) do
 		for slot = 1, GetContainerNumSlots(bag) do
 			local link = GetContainerItemLink(bag, slot)
+			--i could implement basic caching for these searches at some point if speed becomes an issue
 			if link and LIS:Find(link, search) then
 				tinsert(items, slotToIndex(bag, slot, link))
 			end
@@ -39,8 +46,23 @@ local function getItemsInSearch(search, bags)
 	return items	
 end
 
+
+--[[
+	item movement functionality
+--]]
+
 local mover = CreateFrame('Frame')
+
+mover:SetScript('OnHide', function(self, elapsed)
+	--remove lingering references on hide
+	self.destBag = nil
+	self.destSlot = nil
+	self.bags = nil
+	self.items = nil
+end)
+
 mover:SetScript('OnUpdate', function(self, elapsed)
+	--if we're holding an item, then try and place it
 	if self.destBag and self.destSlot then
 		if CursorHasItem() then
 			print('put', self.destBag, self.destSlot)
@@ -52,59 +74,71 @@ mover:SetScript('OnUpdate', function(self, elapsed)
 	end
 	
 	local items = self.items
+	
+	--check to see if there are any remaining items to place
+	--if not, then quit
 	if not(items and #items > 0) then
 		self:Hide()
 		return
 	end
 	
+	--pickup the next item from the stack
 	local fromBag, fromSlot, fromLink = indexToSlot(tremove(items))
 	print('get', fromBag, fromSlot, fromLink)
 	PickupContainerItem(fromBag, fromSlot)
 	
-	local itemPlaced = false
+	--determine where to put the item
+	local foundAHome = false
 	
-	--try and place the item in an empty slot
+	--if stackable
+	--try and place the item in the first unfilled stack we find for the item
+	
+	--try and place the item in the first empty slot we find
 	for _, toBag in ipairs(self.bags) do
 		for toSlot = 1, GetContainerNumSlots(toBag) do
 			local toLink = GetContainerItemLink(toBag, toSlot)
 			if not toLink then
 				self.destBag = toBag
 				self.destSlot = toSlot
-				itemPlaced = true
+				foundAHome = true
 				break
 			end
 		end
-		if itemPlaced then break end
+		
+		if foundAHome then 
+			break
+		end
 	end
 	
-	if not itemPlaced then
-		Bundles:Print('nothing placed')
+	--we were unable to find a spot for the item
+	--so, stop trying to move things
+	if not foundAHome then
 		self:Hide()
 	end
 end)
 
 local function moveItems(items, toBags)
-	print('move items', unpack(items), unpack(toBags))
 	mover.items = items
 	mover.bags = toBags
 	mover:Show()
 end
 
 
---[[ Bundles ]]--
+--[[ 
+	waffle town, usa 
+--]]
 
 Bundles = {}
 
-Bundles._locations = {
+local bagSets = {
 	KEYRING = {KEYRING_CONTAINER},
 	INVENTORY = {BACKPACK_CONTAINER, 1, 2, 3, 4},
 	BANK = {BANK_CONTAINER, 5, 6, 7, 8, 9, 10}
 }
 
 function Bundles:GetBags(type)
-	return self._locations[type:upper()]
+	return bagSets[type:upper()]
 end
-
 
 function Bundles:Move(search, fromLoc, toLoc)
 	if not search then
