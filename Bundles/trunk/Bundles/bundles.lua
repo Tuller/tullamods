751d8396e999d120f0dce88c47df869819606b0f
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-12-02 01:16:31 +0000

    bugfixes

--- Bundles/trunk/Bundles/bundles.lua
+++ Bundles/trunk/Bundles/bundles.lua
@@ -6,33 +6,46 @@
 --]]
 
 local LIS = LibStub('LibItemSearch-1.0')
-local abs = math.abs
-local modf = math.modf
 local tremove = table.remove
 local tinsert = table.insert
 local format = string.format
+local tonumber = tonumber
 
 --[[
 	utility stuff
 --]]
 
-local function slotToIndex(bag, slot, link)
+local function slotToStr(bag, slot, link, count)
 	local link = link or ''
-	return format('%d,%d|%s', bag, slot, link)
+	local count = count or 0
+
+	return format('%d,%d,%d|%s', bag, slot, count, link)
 end
 
-local function indexToSlot(index)
+local function strToSlot(index)
 	if index then
-		local bag, slot, link = index:match('^(%-?%d+)%,(%d+)%|(.*)')
+		local bag, slot, count, link = index:match('^(%-?%d+)%,(%d+)%,(%d+)%|(.*)')
 		if link == '' then link = nil end
 
-		return bag, slot, link
+		return bag, slot, link, tonumber(count)
 	end
 end
 
 local function isSlotLocked(bag, slot)
-	local locked = select(3, GetContainerItemInfo(bag, slot))
+	local texture, count, locked = GetContainerItemInfo(bag, slot)
 	return locked
+end
+
+--attempts to move an item from one slot to another
+--returns true if the slots were not locked, and false otherwise
+local function moveItem(fromBag, fromSlot, toBag, toSlot)
+	if isSlotLocked(fromBag, fromSlot) or isSlotLocked(toBag, toSlot) then
+		return false
+	end
+
+	PickupContainerItem(fromBag, fromSlot)
+	PickupContainerItem(toBag, toSlot)
+	return true
 end
 
 
@@ -40,40 +53,32 @@ end
 	item searching
 --]]
 
-local function pushItem(items, bag, slot, link)
-	tinsert(items, slotToIndex(bag, slot, link))
-end
-
-local function popItem(items)
-	return indexToSlot(tremove(items, 1))
-end
-
-local function getItemsInSearch(search, bags)
-	local items = {}
+local function getSlotsInSearch(search, bags)
+	local slots = {}
 
 	for _, bag in ipairs(bags) do
 		for slot = 1, GetContainerNumSlots(bag) do
-			local link = GetContainerItemLink(bag, slot)
-			--i could implement basic caching for these searches at some point if speed becomes an issue
+			local texture, count, locked, quality, readable, lootable, link = GetContainerItemInfo(bag, slot)
 			if link and LIS:Find(link, search) then
-				pushItem(items, bag, slot, link)
+				tinsert(slots, slotToStr(bag, slot, link, count))
 			end
 		end
 	end
 
-	return items
+	return slots
 end
 
-local function getItems(bags)
-	local items = {}
+local function getSlots(bags)
+	local slots = {}
 
 	for _, bag in ipairs(bags) do
 		for slot = 1, GetContainerNumSlots(bag) do
-			pushItem(items, bag, slot, GetContainerItemLink(bag, slot))
+			local texture, count, locked, quality, readable, lootable, link = GetContainerItemInfo(bag, slot)
+			tinsert(slots, slotToStr(bag, slot, link, count))
 		end
 	end
 
-	return items
+	return slots
 end
 
 --[[
@@ -83,18 +88,15 @@ end
 local mover = CreateFrame('Frame')
 mover:Hide()
 mover.DELAY = 0.05
-mover.MAX_NO_MOVES = 50
 
 --remove lingering references on hide
 mover:SetScript('OnShow', function(self)
 	self.elapsed = -1
-	self.noMoveCount = 0
 end)
 
 mover:SetScript('OnHide', function(self, elapsed)
-	self.fromItems = nil
-	self.toItems = nil
-	collectgarbage()
+	self.fromSlots = nil
+	self.toSlots = nil
 end)
 
 mover:SetScript('OnUpdate', function(self, elapsed)
@@ -104,48 +106,67 @@ mover:SetScript('OnUpdate', function(self, elapsed)
 	end
 	self.elapsed = self.DELAY
 
-	--pickup the next item from the stack
-	local fromBag, fromSlot, fromLink = popItem(self.fromItems)
+	--pickup the next item from the stack, quit if there are no more
+	local fromBag, fromSlot, fromLink, fromCount = strToSlot(self.fromSlots[1])
 	if not(fromBag and fromSlot) then
 		self:Hide()
 		return
 	end
 
-	--try and place the item in the first empty slot we find
-	local itemPlaced = false
-	local toItems = self.toItems
-	local newItems = {} --delicious garbage
+	local placed, locked = false, false
+	local maxCount = select(8, GetItemInfo(fromLink))
 
-	while next(toItems) do
-		local toBag, toSlot, toLink = popItem(toItems)
+	--try to place the item in the first stack that can fit it
+	if fromCount < maxCount then
+		for i, slotInfo in ipairs(self.toSlots) do
+			local toBag, toSlot, toLink, toCount = strToSlot(slotInfo)
 
-		if not(itemPlaced or toLink or isSlotLocked(fromBag, fromSlot) or isSlotLocked(toBag, toSlot)) then
-			PickupContainerItem(fromBag, fromSlot)
-			PickupContainerItem(toBag, toSlot)
-			pushItem(newItems, toBag, toSlot, fromLink) --this was the thing I did not learn from the sorting addons: you need to account for server delays by recording your own moves
-
-			itemPlaced = true
-		else
-			pushItem(newItems, toBag, toSlot, toLink)
+			--look for the same item and a different slot
+			if (fromLink == toLink) and not(toBag == fromBag and toSlot == fromSlot) then
+				--we've found a good place to put the item, so try to move it there
+				if (fromCount + toCount) <= maxCount then
+					if moveItem(fromBag, fromSlot, toBag, toSlot) then
+						self.toSlots[i] = slotToStr(toBag, toSlot, fromLink, fromCount + toCount)
+						placed = true
+					else
+						locked = true
+					end
+					break
+				end
+			end
 		end
 	end
-	self.toItems = newItems
 
-	--we were not able to move the item, so
-	if not itemPlaced then
-		pushItem(self.fromItems, fromBag, fromSlot, fromLink)
-		self.noMoveCount = self.noMoveCount + 1
+	--try to place the item in the first empty slot we find
+	if not (itemPlaced or slotLocked) then
+		for i, slotInfo in ipairs(self.toSlots) do
+			local toBag, toSlot, toLink, toCount = strToSlot(slotInfo)
+
+			--we've found a good place to put the item, so try to move it there
+			if not(toLink or (toBag == fromBag and toSlot == fromSlot)) then
+				if moveItem(fromBag, fromSlot, toBag, toSlot) then
+					self.toSlots[i] = slotToStr(toBag, toSlot, fromLink, fromCount)
+					placed = true
+				else
+					locked = true
+				end
+			end
+		end
 	end
 
-	--infinite loop prevention
-	if self.noMoveCount > self.MAX_NO_MOVES then
+	--the item was placed, so remove it from the list of items to move
+	if placed then
+		tremove(self.fromSlots, 1)
+	--the item was not placed, and the slot was not locked
+	--therefore, we have no more remaining slots, so quit
+	elseif not locked then
 		self:Hide()
 	end
 end)
 
-local function moveItems(fromItems, toItems)
-	mover.fromItems = fromItems
-	mover.toItems = toItems
+local function moveItems(fromSlots, toSlots)
+	mover.fromSlots = fromSlots
+	mover.toSlots = toSlots
 	mover:Show()
 end
 
@@ -157,10 +178,10 @@ end
 Bundles = {}
 
 local bagSets = {
-	all = {KEYRING_CONTAINER, BACKPACK_CONTAINER, 1, 2, 3, 4, BANK_CONTAINER, 5, 6, 7, 8, 9, 10},
+	all = {KEYRING_CONTAINER, BACKPACK_CONTAINER, 1, 2, 3, 4, BANK_CONTAINER, 5, 6, 7, 8, 9, 10, 11},
 	keyring = {KEYRING_CONTAINER},
 	inventory = {BACKPACK_CONTAINER, 1, 2, 3, 4},
-	bank = {BANK_CONTAINER, 5, 6, 7, 8, 9, 10}
+	bank = {BANK_CONTAINER, 5, 6, 7, 8, 9, 10, 11}
 }
 
 --a few aliases
@@ -186,10 +207,10 @@ function Bundles:Find(search, loc)
 		return
 	end
 
-	local items = getItemsInSearch(search, bags)
-	if next(items) then
-		while next(items) do
-			self:Print(popItem(items))
+	local slots = getSlotsInSearch(search, bags)
+	if next(slots) then
+		for i, slotInfo in ipairs(slots) do
+			self:Print(strToSlot(slotInfo))
 		end
 	else
 		self:Print(format('No items were found in %s for %s', search, loc))
@@ -214,13 +235,13 @@ function Bundles:Move(search, fromLoc, toLoc)
 		return
 	end
 
-	local fromItems = getItemsInSearch(search, fromBags)
-	if #fromItems < 0 then
+	local fromSlots = getSlotsInSearch(search, fromBags)
+	if #fromSlots < 0 then
 		self:Print(format('No items were found in %s for %s', search, fromLoc))
 		return
 	end
 
-	moveItems(fromItems, getItems(toBags))
+	moveItems(fromSlots, getSlots(toBags))
 end
 
 function Bundles:Print(...)
