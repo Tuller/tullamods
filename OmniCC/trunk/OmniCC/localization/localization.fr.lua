3f7d92e62e84b45b8a2cea63be77acd867180a7f
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2010-01-05 22:29:01 +0000

    updated french localization (thanks dokcharlie) toc bump

--- OmniCC/trunk/OmniCC/localization/localization.fr.lua
+++ OmniCC/trunk/OmniCC/localization/localization.fr.lua
@@ -1,11 +1,11 @@
---[[
+﻿--[[
 	OmniCC Localization File
-		French by Mordroba
+		French by TrAsHeR
 --]]
 
 if GetLocale() == 'frFR' then
 	OMNICC_LOCALS = {}
 	local L = OMNICC_LOCALS
-	L.UpgradeIncompatible = "Mise \195\160 jour \195\160 partir d'une version incompatible. Param\195\168tres par d\195\169faut charg\195\169s"
+	L.UpgradeIncompatible = "Mise à jour depuis une version incompatible. Les paramètres par défaut ont été chargés."
 	L.Updated = "Updated to v%s"
 end
--- OmniCC/trunk/OmniCC_Options/localization/localization.fr.lua
+++ OmniCC/trunk/OmniCC_Options/localization/localization.fr.lua
@@ -1,4 +1,4 @@
---[[
+﻿--[[
 	OmniCC Options Localizion
 		French by Mordroba
 --]]
@@ -7,19 +7,19 @@ if GetLocale() == 'frFR' then
 	local L = OMNICC_LOCALS
 	L.Thin = 'Fin'
 	L.Thick = 'Epais'
-	L.ShowModels = 'Montrer mod\195\168les de cooldowns'
+	L.ShowModels = 'Afficher les modèles de cooldowns'
 	L.ShowPulse = 'Montrer l\'impulsion'
 	L.UseMMSS = 'Utiliser le formatage MM:SS'
-	L.MinDuration = 'Seuil de dur\195\169e'
+	L.MinDuration = 'Seuil de durée'
 	L.MinScale = 'Taille du seuil'
 	L.Display = 'Affichage'
 	L.FontFace = 'Type de police'
-	L.FontOutline = 'Contour de police'
-	L.FontSize = 'Taille de police'
+	L.FontOutline = 'Contour de la police'
+	L.FontSize = 'Taille de la police'
 	L.Font = 'Police'
-	L.ColorsAndScaling = 'Couleurs et \195\169chelle'
+	L.ColorsAndScaling = 'Couleurs et échelle'
 	L.UnderFiveSeconds = 'Moins de 5s'
 	L.OptionsTitle = 'Options d\'OmniCC'
 	L.MinEffectDuration = 'Effet de fin du seuil'
-	L.ShowTenthsOfSeconds = 'Afficher dixi\195\168mes de secondes'
+	L.ShowTenthsOfSeconds = 'Afficher dixièmes de secondes'
 end
